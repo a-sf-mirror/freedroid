5248064211cf901e265a6d87edcfc70a3c97381a
ahuillet <ahuillet@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2008-11-08 10:08:53 +0000

    non fatal error messages for savegame system - do not abort loading when there is an array size mismatch.
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@1270 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/saveloadgame.c
+++ src/saveloadgame.c
@@ -53,7 +53,8 @@ jmp_buf saveload_jmpbuf;
 #define WrapErrorMessage(a, b, c, d, ...) do { \
 	ErrorMessage(a,b,c,IS_WARNING_ONLY, ##__VA_ARGS__);\
     	GiveMouseAlertWindow(_("An error occured when trying to load the savegame.\nA common reason for this is that the game has been updated to a newer version since the save was made, in which case the savegame is very likely not compatible.\nHowever if you see this message and you have not updated the game, make sure to report this to the developers.\nThanks!"));\
-    	longjmp(saveload_jmpbuf, 1);\
+        if (d==IS_FATAL)\
+    	    longjmp(saveload_jmpbuf, 1);\
 } while (0)
 
 
@@ -726,7 +727,7 @@ while ( *erunp != '>' ) erunp ++;\
 *erunp = '\0';\
 nb = atoi(runp);\
 *erunp = '>';\
-if ( nb != size )  WrapErrorMessage ( __FUNCTION__, "Size mismatch for array %s, %d in file, %d in game\n", PLEASE_INFORM, IS_FATAL, tag, nb, size);\
+if ( nb != size )  WrapErrorMessage ( __FUNCTION__, "Size mismatch for array %s, %d in file, %d in game\n", NO_NEED_TO_INFORM, IS_WARNING_ONLY, tag, nb, size);\
 for ( i = 0; i < (nb <= size ? nb : size); i ++)\
 	{\
 	char str[10];\
