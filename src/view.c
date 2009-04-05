b5fab718c6b81d7ace0f9177816f8c067ae44435
ahuillet <ahuillet@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2009-04-05 09:33:22 +0000

    fix conceptual errors regarding blue and red lines in the level editor. The red line indicates the level border: end of the level. The blue line indicates the jump position: when will Tux be teleported to the next level.
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@1878 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/view.c
+++ src/view.c
@@ -1629,16 +1629,16 @@ draw_grid_on_the_floor (int mask)
     SetCurrentFont (PreviousFont);
 
     // display level's interface edges
-    skew_and_blit_line ( our_level->jump_threshold_west, 0, our_level->jump_threshold_west, our_level->ylen, 0x0000FF, 3);
-    skew_and_blit_line ( our_level->xlen - our_level->jump_threshold_east, 0, our_level->xlen - our_level->jump_threshold_east, our_level->ylen, 0x0000FF, 3);
-    skew_and_blit_line ( 0, our_level->jump_threshold_north, our_level->xlen, our_level->jump_threshold_north, 0x0000FF, 3);
-    skew_and_blit_line ( 0, our_level->ylen - our_level->jump_threshold_south, our_level->xlen, our_level->ylen - our_level->jump_threshold_south, 0x0000FF, 3);
+    skew_and_blit_line (our_level->jump_threshold_west/2.0, 0, our_level->jump_threshold_west/2, our_level->ylen, 0x0000FF, 3);
+    skew_and_blit_line (our_level->xlen - our_level->jump_threshold_east/2.0, 0, our_level->xlen - our_level->jump_threshold_east/2.0, our_level->ylen, 0x0000FF, 3);
+    skew_and_blit_line (0, our_level->jump_threshold_north/2.0, our_level->xlen, our_level->jump_threshold_north/2.0, 0x0000FF, 3);
+    skew_and_blit_line (0, our_level->ylen - our_level->jump_threshold_south/2.0, our_level->xlen, our_level->ylen - our_level->jump_threshold_south/2.0, 0x0000FF, 3);
 
     // now display the level borders
-    skew_and_blit_line ( our_level->jump_threshold_west/2.0, 0, our_level->jump_threshold_west/2.0, our_level->ylen, 0xFF0000, 3);
-    skew_and_blit_line ( our_level->xlen - our_level->jump_threshold_east/2.0, 0, our_level->xlen - our_level->jump_threshold_east/2.0, our_level->ylen, 0xFF0000, 3);
-    skew_and_blit_line ( 0, our_level->jump_threshold_north/2.0, our_level->xlen, our_level->jump_threshold_north/2.0, 0xFF0000, 3);
-    skew_and_blit_line ( 0, our_level->ylen - our_level->jump_threshold_south/2.0, our_level->xlen, our_level->ylen - our_level->jump_threshold_south/2.0, 0xFF0000, 3);
+    skew_and_blit_line (0, 0, 0, our_level->ylen, 0xFF0000, 3);
+    skew_and_blit_line (our_level->xlen, 0, our_level->xlen, our_level->ylen, 0xFF0000, 3);
+    skew_and_blit_line (0, 0, our_level->xlen, 0, 0xFF0000, 3);
+    skew_and_blit_line (0, our_level->ylen, our_level->xlen, our_level->ylen, 0xFF0000, 3);
 }
 
 /* -----------------------------------------------------------------
