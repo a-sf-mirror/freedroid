fddd86c39324e630143c0b7bf6943816c4691294
fluzz <fluzz@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2009-09-09 21:43:12 +0000

    Remove the prototype of a no more existing function
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@1975 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/proto.h
+++ src/proto.h
@@ -328,7 +328,6 @@ void glue_obstacles_to_floor_tiles_for_level(int level_num);
 void ResolveMapLabelOnShip(const char *MapLabel, location * PositionPointer);
 void CollectAutomapData(void);
 int smash_obstacle(float x, float y, int level);
-int update_level_pos(level **, float *, float *);
 Uint16 GetMapBrick(level *, float, float);
 void CountNumberOfDroidsOnShip(void);
 int LoadShip(char *filename);
