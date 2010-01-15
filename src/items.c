d29c37089c1915c4c3e1f08a4990f6ff6907faca
fluzz <fluzz@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2010-01-15 22:20:00 +0000

    Avoid to drop 0 valuable circuits
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@2293 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/items.c
+++ src/items.c
@@ -590,7 +590,13 @@ void DropRandomItem(int level_num, float x, float y, int class, int ForceMagical
 	// simple, so we do this first.
 	//
 	if ((DropDecision > 100 - GOLD_DROP_PERCENTAGE)) {
-		DropItemAt(GetItemIndexByName("Valuable Circuits"), level_num, x, y, -1, -1, 10 * class + MyRandom(9));
+		// If class == 0, we want to avoid to drop 0 valuable circuits
+		int how_many = 0;
+		do {
+			how_many = 10 * class + MyRandom(9);
+		} while (how_many == 0);
+		
+		DropItemAt(GetItemIndexByName("Valuable Circuits"), level_num, x, y, -1, -1, how_many);
 	}
 
 	if ((DropDecision < ITEM_DROP_PERCENTAGE)) {
