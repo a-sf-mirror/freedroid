e0390bec9e14cec63f967ec71a29ad5b4247982c
stedevil <stedevil@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2010-02-11 03:01:58 +0000

    updates to gun and sword glue.sh files
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@2403 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- graphics/tux_motion_parts/sword_motion/glue_all_sword_motion.sh
+++ graphics/tux_motion_parts/sword_motion/glue_all_sword_motion.sh
@@ -2,22 +2,23 @@
 gluem -n -d 16 -i iso_boots1
 gluem -n -d 16 -i iso_robe
 gluem -n -d 16 -i iso_torso
-gluem -n -d 16 -i iso_mace
-gluem -n -d 16 -i iso_head
-gluem -n -d 16 -i iso_sword
 gluem -n -d 16 -i iso_weaponarm
 gluem -n -d 16 -i iso_armour1
-gluem -n -d 16 -i iso_iron_pipe
 gluem -n -d 16 -i iso_helm1
 gluem -n -d 16 -i iso_feet
 
-cd shield_items
-gluem -n -d 16 -i iso_standard_shield
+cd ../weapons
+gluem -n -d 16 -i iso_mace
+gluem -n -d 16 -i iso_head
+gluem -n -d 16 -i iso_sword
+gluem -n -d 16 -i iso_iron_pipe
+cd ../shields
+gluem -n -d 16 -i iso_heavy_shield
 gluem -n -d 16 -i iso_shieldarm
 gluem -n -d 16 -i iso_buckler
-gluem -n -d 16 -i iso_small_shield
-gluem -n -d 16 -i iso_large_shield
-cd ..
+gluem -n -d 16 -i iso_standard_shield
+gluem -n -d 16 -i iso_riot_shield
+cd ../sword_motion
 
 echo "All the sword_motion files should be glued properly now."
 echo " "
