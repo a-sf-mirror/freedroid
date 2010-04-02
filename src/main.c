567f4d3b19ec5b6edaad14a4b5b14e2ff37c7cd0
stedevil <stedevil@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2010-04-02 23:12:49 +0000

    Split automatic shutdown text into 2 lines to fit on small screens (Bug ID: 2978970)
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@2573 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/main.c
+++ src/main.c
@@ -379,7 +379,8 @@ void UpdateCountersForThisFrame()
 		if (Me.energy - lose_life < 1 && Me.paralyze_duration == 0) {
 			DoSkill(get_program_index_with_name("Emergency shutdown"),
 				calculate_program_heat_cost(get_program_index_with_name("Emergency shutdown")));
-			SetNewBigScreenMessage(_("Automatically shutting down due to near lethal overheat"));
+			SetNewBigScreenMessage(_("due to near lethal overheating"));
+			SetNewBigScreenMessage(_("Automatically shutting down"));
 			lose_life = (Me.temperature - Me.max_temperature) * latest_frame_time / 10;
 		}
 		if (lose_life > 0) {
