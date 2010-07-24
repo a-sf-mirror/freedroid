90b874272cdf6128553e00ae183d8fa772fc25d4
ahuillet <ahuillet@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2010-07-24 08:57:08 +0000

    Add NEIGHBOR_ID() to retrieve the neighbors of a level. Patch by Samuel Pitoiset.
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@3096 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/global.h
+++ src/global.h
@@ -157,14 +157,15 @@ EXTERN list_head_t visible_level_list;
 EXTERN int gps_transform_map_dirty_flag;
 
 #define NEIGHBOR_IDX(x,len) ( ( (x) < 0) ? (0) : ( ( (x) < (len) ) ? (1) : (2) ) )
-#define NEIGHBOR_ID_NW(lvl) ( level_neighbors_map[lvl][0][0] ? level_neighbors_map[lvl][0][0]->lvl_idx : -1 )
-#define NEIGHBOR_ID_N(lvl)  ( level_neighbors_map[lvl][0][1] ? level_neighbors_map[lvl][0][1]->lvl_idx : -1 )
-#define NEIGHBOR_ID_NE(lvl) ( level_neighbors_map[lvl][0][2] ? level_neighbors_map[lvl][0][2]->lvl_idx : -1 )
-#define NEIGHBOR_ID_W(lvl)  ( level_neighbors_map[lvl][1][0] ? level_neighbors_map[lvl][1][0]->lvl_idx : -1 )
-#define NEIGHBOR_ID_E(lvl)  ( level_neighbors_map[lvl][1][2] ? level_neighbors_map[lvl][1][2]->lvl_idx : -1 )
-#define NEIGHBOR_ID_SW(lvl) ( level_neighbors_map[lvl][2][0] ? level_neighbors_map[lvl][2][0]->lvl_idx : -1 )
-#define NEIGHBOR_ID_S(lvl)  ( level_neighbors_map[lvl][2][1] ? level_neighbors_map[lvl][2][1]->lvl_idx : -1 )
-#define NEIGHBOR_ID_SE(lvl) ( level_neighbors_map[lvl][2][2] ? level_neighbors_map[lvl][2][2]->lvl_idx : -1 )
+#define NEIGHBOR_ID(lvl, x, y) (level_neighbors_map[lvl][y][x] ? level_neighbors_map[lvl][y][x]->lvl_idx : -1)
+#define NEIGHBOR_ID_NW(lvl) NEIGHBOR_ID(lvl, 0, 0)
+#define NEIGHBOR_ID_N(lvl)  NEIGHBOR_ID(lvl, 1, 0)
+#define NEIGHBOR_ID_NE(lvl) NEIGHBOR_ID(lvl, 2, 0)
+#define NEIGHBOR_ID_W(lvl)  NEIGHBOR_ID(lvl, 0, 1)
+#define NEIGHBOR_ID_E(lvl)  NEIGHBOR_ID(lvl, 2, 1)
+#define NEIGHBOR_ID_SW(lvl) NEIGHBOR_ID(lvl, 0, 2)
+#define NEIGHBOR_ID_S(lvl)  NEIGHBOR_ID(lvl, 1, 2)
+#define NEIGHBOR_ID_SE(lvl) NEIGHBOR_ID(lvl, 2, 2)
 
 #undef EXTERN
 #ifdef _light_c
