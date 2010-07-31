3c395a694cd5a7c52138616574500181400b44d5
ahuillet <ahuillet@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2010-07-31 10:54:22 +0000

    Balance the bonuses, prices, and material requirements of item add-ons. Also, tune descriptions and names a little bit. Patch by Ari Mustonen.
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@3130 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- map/addon_specs.lua
+++ map/addon_specs.lua
@@ -1,28 +1,28 @@
 addon{
 name = _"Linarian power crank",
-upgrade_cost = 45,
+upgrade_cost = 30,
 require_socket = "mechanical",
 require_item = "melee weapon",
-bonuses = { strength = 5 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { strength = 10 },
+materials = { ["Entropy Inverter"] = 10 }
 }
 
 addon{
-name = _"Tungsten Spikes",
-upgrade_cost = 45,
+name = _"Tungsten spikes",
+upgrade_cost = 30,
 require_socket = "mechanical",
 require_item = "melee weapon",
 bonuses = { damage = 3 },
-materials = { ["Tachyon Condensator"] = 1 }
+materials = { ["Plasma Transistor"] = 10 }
 }
 
 addon{
-name = _"Tinfoil Patch",
-upgrade_cost = 45,
+name = _"Tinfoil patch",
+upgrade_cost = 50,
 require_socket = "mechanical",
 require_item = "armor",
-bonuses = { cooling = 3, armor = 3 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { cooling = 5, armor = 5 },
+materials = { ["Entropy Inverter"] = 20, ["Plasma Transistor"] = 10 }
 }
 
 addon{
@@ -30,115 +30,106 @@ name = _"Laser sight",
 upgrade_cost = 45,
 require_socket = "universal",
 require_item = "ranged weapon",
-bonuses = { dexterity = 12 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { dexterity = 15 },
+materials = { ["Antimatter-Matter Converter"] = 3, ["Plasma Transistor"] = 10 }
 }
 
 addon{
-name = _"Exoskeletal Joint",
-upgrade_cost = 45,
+name = _"Exoskeletal joint",
+upgrade_cost = 80,
 require_socket = "universal",
 require_item = "armor",
-bonuses = { physique = 5, strength = 5, dexterity = -3 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { physique = 10, strength = 15, dexterity = -2 },
+materials = { ["Superconducting Relay Unit"] = 10, ["Entropy Inverter"] = 10 }
 }
 
 addon{
 name = _"Heatsink",
-upgrade_cost = 45,
+upgrade_cost = 50,
 require_socket = "mechanical",
 require_item = "armor",
-bonuses = { cooling_rate = 1 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { cooling = 10 },
+materials = { ["Entropy Inverter"] = 10 }
 }
 
 addon{
-name = _"Peltier Element",
-upgrade_cost = 45,
+name = _"Peltier element",
+upgrade_cost = 60,
 require_socket = "electric",
 require_item = "armor",
 bonuses = { cooling_rate = 3 },
-materials = { ["Tachyon Condensator"] = 1 }
+materials = { ["Tachyon Condensator"] = 15 }
 }
 
 addon{
-name = _"Heat Buffer",
-upgrade_cost = 45,
-require_socket = "mechanical",
-require_item = "armor",
-bonuses = { cooling = 4 },
-materials = { ["Tachyon Condensator"] = 1 }
-}
-
-addon{
-name = _"Steel Mesh",
+name = _"Steel mesh",
 upgrade_cost = 45,
 require_socket = "mechanical",
 require_item = "armor",
 bonuses = { armor = 10 },
-materials = { ["Tachyon Condensator"] = 1 }
+materials = { ["Antimatter-Matter Converter"] = 5, ["Tachyon Condensator"] = 5 }
 }
 
 addon{
-name = _"Shock Discharger",
-upgrade_cost = 45,
+name = _"Shock discharger",
+upgrade_cost = 100,
 require_socket = "electric",
 require_item = "melee weapon",
-bonuses = { damage = 5 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { damage = 5, slow_enemy = 1 },
+materials = { ["Superconducting Relay Unit"] = 20, ["Tachyon Condensator"] = 5 }
 }
 
 addon{
 name = _"Silencer",
-upgrade_cost = 45,
+upgrade_cost = 30,
 require_socket = "mechanical",
 require_item = "ranged weapon",
 bonuses = { damage = 2 },
-materials = { ["Tachyon Condensator"] = 1 }
+materials = { ["Entropy Inverter"] = 25, ["Plasma Transistor"] = 10 }
 }
 
 addon{
 name = _"Coprocessor",
-upgrade_cost = 45,
+upgrade_cost = 80,
 require_socket = "electric",
-bonuses = { all_attributes = 1 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { all_attributes = 3 },
+materials = { ["Tachyon Condensator"] = 5, ["Plasma Transistor"] = 15 }
 }
 
 addon{
 name = _"Pedometer",
-upgrade_cost = 45,
+upgrade_cost = 40,
 require_socket = "electric",
 require_item = "boots",
-bonuses = { dexterity = 4 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { dexterity = 10 },
+materials = { ["Superconducting Relay Unit"] = 3, ["Plasma Transistor"] = 10 }
 }
 
 addon{
-name = _"Foot Warmer",
-upgrade_cost = 45,
+name = _"Foot warmer",
+upgrade_cost = 50,
 require_socket = "electric",
 require_item = "boots",
-bonuses = { physique = 7, cooling = -2 },
-materials = { ["Tachyon Condensator"] = 1 }
+bonuses = { physique = 10, cooling = -3 },
+materials = { ["Entropy Inverter"] = 20, ["Plasma Transistor"] = 5 }
 }
 
 addon{
-name = _"Circuit Jammer",
-upgrade_cost = 45,
+name = _"Circuit jammer",
+upgrade_cost = 200,
 require_socket = "electric",
 require_item = "melee weapon",
-bonuses = { paralyze_enemy = 2 },
-materials = { ["Tachyon Condensator"] = 2, ["Plasma Transistor"] = 1 }
+bonuses = { paralyze_enemy = 1 },
+materials = { ["Antimatter-Matter Converter"] = 8, ["Tachyon Condensator"] = 8 }
 }
 
 addon{
-name = _"Sensor Disruptor",
-upgrade_cost = 45,
+name = _"Sensor disruptor",
+upgrade_cost = 100,
 require_socket = "universal",
 require_item = "melee weapon",
-bonuses = { slow_enemy = 3 },
-materials = { ["Tachyon Condensator"] = 2 }
+bonuses = { slow_enemy = 5 },
+materials = { ["Antimatter-Matter Converter"] = 8, ["Superconducting Relay Unit"] = 8, ["Tachyon Condensator"] = 10 }
 }
 
 addon{
@@ -151,7 +142,7 @@ materials = { ["Entropy Inverter"] = 15, ["Plasma Transistor"] = 5 }
 }
 
 addon{
-name = _"Brain Stimulator",
+name = _"Brain stimulator",
 upgrade_cost = 45,
 require_socket = "electric",
 require_item = "armor",
