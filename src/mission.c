989c69ccca8d83672b61591c90143997bde96779
ahuillet <ahuillet@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2010-10-30 10:01:08 +0000

    Display statistics in quest log. Patch by Miles.
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@3374 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/mission.c
+++ src/mission.c
@@ -143,6 +143,190 @@ void quest_browser_append_mission_info(const char *mis_name, int full_descriptio
 
 };				// void quest_browser_append_mission_info ( int mis_num )
 
+//comparison function for droid names
+static int cmp_droid_names(const void *ptr1, const void *ptr2){
+	const char *ch1 =  Druidmap[*(int *)ptr1].default_short_description;
+	const char *ch2 =  Druidmap[*(int *)ptr2].default_short_description;
+	return strcmp(ch1, ch2);
+}
+
+//function that assembles and prints statistics about the player
+static void print_statistics(void)
+{
+	struct auto_string *stats_buffer = alloc_autostr(5000);
+	int statistics_browser_lines_needed[Number_Of_Droid_Types + 2];
+	int display = 0;                //display location
+	int srt[Number_Of_Droid_Types]; //used for alphabetizing droid names in display
+	int model;                      //increment Druidmap
+	int rate;                       //used for finding best/worst Takeover rate
+
+	//totals, bests & worsts
+	int total_destroyed = 0;
+	int total_damage_dealt = 0;
+	int total_takeover_success = 0;
+	int fav_destroyed = -1;
+	int fav_capture_target = -1;
+	int fav_capture_success_rate = 0;
+	int worst_capture_target = -1;
+	int worst_capture_success_rate = 100;
+
+	// Grab information out of the statistics arrays
+	for (model = 0; model < Number_Of_Droid_Types; model++) {
+		total_takeover_success += Me.TakeoverSuccesses[model];
+	        total_destroyed += Me.destroyed_bots[model];
+	        total_damage_dealt += Me.damage_dealt[model];
+		if (Me.destroyed_bots[model] > Me.destroyed_bots[fav_destroyed])
+			fav_destroyed = model;
+		if (Me.TakeoverSuccesses[model] + Me.TakeoverFailures[model]) {
+			rate = (100 * Me.TakeoverSuccesses[model])/ (Me.TakeoverSuccesses[model] + Me.TakeoverFailures[model]);
+			if (rate > fav_capture_success_rate ) {
+				fav_capture_target = model;
+				fav_capture_success_rate = rate;
+			}
+			if (rate < worst_capture_success_rate) {
+				worst_capture_target = model;
+				worst_capture_success_rate = rate;
+			}
+		}
+		srt[model] = model; //set to un-alphabetized order from freedroid.droid_archetypes
+	}
+
+	//Assemble Output
+	autostr_append(stats_buffer, _("\1Parafunken Statistics\2\n"));
+
+	// This is where we display grand total statistics
+	autostr_append(stats_buffer, _("\1Overview\2\n"));
+	statistics_browser_lines_needed[display] = get_lines_needed(stats_buffer->value, mission_description_rect, 1);
+	if (stats_display[display++]) {
+		autostr_append(stats_buffer, _("Distance Traveled: \3%.1fm\2\n"), Me.meters_traveled);
+		autostr_append(stats_buffer, _("Destroyed Enemies: \3%i\2\n"), total_destroyed);
+		autostr_append(stats_buffer, _("Captured Enemies: \3%i\2\n"), total_takeover_success);
+		autostr_append(stats_buffer, _("Damage Dealt: \3%i\2\n"), total_damage_dealt);
+	}
+
+	// This is where we display statistics about the best and worst relationships with bots:
+	// Attacking -> type most destroyed or killed (show #destroyed, #damage)
+	// Takeover  -> type with highest takeover % (%rate, # attempts)
+	// Defending -> type most damage done to Tux (# damage)
+	// Failed Takeovers -> lowest takeover % (% rate, # attempts)
+	autostr_append(stats_buffer, _("\1Preferences\2\n"));
+	statistics_browser_lines_needed[display] = get_lines_needed(stats_buffer->value, mission_description_rect, 1);
+	if (stats_display[display++]) {
+		if (fav_destroyed != -1) {
+			autostr_append(stats_buffer, _("Attacking: \3%s\2 (\3%i\2 "), 
+				Druidmap[fav_destroyed].default_short_description, Me.destroyed_bots[fav_destroyed]);
+			if (Druidmap[fav_destroyed].is_human)
+				autostr_append(stats_buffer, _("killed,"));
+			else
+				autostr_append(stats_buffer, _("destroyed,"));
+			autostr_append(stats_buffer, _(" \3%i\2 DP)\n"), Me.damage_dealt[fav_destroyed]);
+		} else
+			autostr_append(stats_buffer, _("Attacking: no droids destroyed\n"));
+
+		if (fav_capture_target != -1)
+			autostr_append(stats_buffer, _("Takeover: \3%s\2 (\3%i%%\2 rate, \3%i\2 attempts)\n"),
+					Druidmap[fav_capture_target].default_short_description, fav_capture_success_rate,
+					Me.TakeoverSuccesses[fav_capture_target] + Me.TakeoverFailures[fav_capture_target]);
+		else
+			autostr_append(stats_buffer, _("Takeover: none successful\n"));
+
+
+		if (worst_capture_target != -1)
+			autostr_append(stats_buffer, _("Failed Takeover: \3%s\2 (\3%i%%\2 rate, \3%i\2 attempts)\n"),
+					Druidmap[worst_capture_target].default_short_description, worst_capture_success_rate,
+					Me.TakeoverSuccesses[worst_capture_target] + Me.TakeoverFailures[worst_capture_target]);
+		else
+			autostr_append(stats_buffer, _("Failed Takeover: no failures\n"));
+	}
+
+	//This sorts the droid types by default model name (from freedroid.droid_archetypes):
+	//srt[] will hold the Druidmap[] indices in this order 
+	qsort(srt, Number_Of_Droid_Types, sizeof(srt[0]), cmp_droid_names);
+
+	// This is where we display statistics about each bot/human type we have interacted with:
+	// Killed/Destroyed: humans are 'killed', bots are 'destroyed', show #
+	// Takeover: # successes, # failures, % success/total (hidden for human models)
+	// Damage: # damage dealt, # damage recieved, % delt/total
+	int counter;
+	for (counter = 0; counter < Number_Of_Droid_Types; counter++) {
+		model = srt[counter];
+		//only show bot types the player has interacted with:
+		if (Me.damage_dealt[model] || Me.TakeoverSuccesses[model] || Me.TakeoverFailures[model] || Me.destroyed_bots[model]) {
+			autostr_append(stats_buffer, "\1%s\2\n", Druidmap[model].default_short_description);
+			statistics_browser_lines_needed[display] = get_lines_needed(stats_buffer->value, mission_description_rect, 1);
+			if (stats_display[display++]) {
+				if (Druidmap[model].is_human) {
+					autostr_append(stats_buffer, _("Killed: \3%d\2\n"), Me.destroyed_bots[model]);
+				} else {
+					autostr_append(stats_buffer, _("Destroyed: \3%d\2\n"), Me.destroyed_bots[model]);
+					autostr_append(stats_buffer, _("Takeovers: \3%d\2/\3%d\2/\3"), Me.TakeoverSuccesses[model], Me.TakeoverFailures[model]);
+					if (Me.TakeoverSuccesses[model] + Me.TakeoverFailures[model])
+						autostr_append(stats_buffer, _("%d%%\2"),
+							(100 * Me.TakeoverSuccesses[model])/(Me.TakeoverSuccesses[model] + Me.TakeoverFailures[model]));
+					else
+						autostr_append(stats_buffer, _(" 0%%\2"));
+					autostr_append(stats_buffer, _(" (success/fail/ratio)\n"));
+				}
+				autostr_append(stats_buffer, _("Damage: \3%d\2 (dealt)\n"), Me.damage_dealt[model]);
+			}
+		} else {
+			statistics_browser_lines_needed[display++] = -1;
+		}
+	}
+
+	//Print all to screen
+	mission_list_offset = (FontHeight(GetCurrentFont()) * TEXT_STRETCH)
+	    * (mission_list_scroll_override_from_user) * 1.00;
+	DisplayText(stats_buffer->value, mission_description_rect.x,
+			    mission_description_rect.y - mission_list_offset, &mission_description_rect, 1);
+
+	// Now it's time to display some short/long symbols in front
+	// of each of the sections.
+	//
+	for (display = 0; display < Number_Of_Droid_Types + 2; display++) {
+		if (statistics_browser_lines_needed[display] == -1)
+			continue;
+
+		SDL_Rect *rect_short = &(AllMousePressButtons[QUEST_BROWSER_ITEM_SHORT_BUTTON].button_rect);
+		SDL_Rect *rect_long = &(AllMousePressButtons[QUEST_BROWSER_ITEM_LONG_BUTTON].button_rect);
+
+		// At first we bring the short/long buttons into position.
+		// This position might be well off the screen.  That's no
+		// problem, because we won't blit the thing in this case
+		// anyway.
+		rect_short->y =
+		    mission_description_rect.y - mission_list_offset +
+		    (FontHeight(GetCurrentFont()) * 1.0) * (statistics_browser_lines_needed[display] -1) - 2;
+		rect_short->x = mission_description_rect.x - 26;
+		rect_long->y = rect_short->y;
+		rect_long->x = rect_short->x;
+
+		// Now we check if the y coordinate of the buttons are 
+		// somewhat reasonable or not.  For those buttons that are
+		// off the screen, things are simple, because then we can
+		// skip the rest of this pass of the loop.
+		//
+		if (rect_short->y <= mission_description_rect.y - 4)
+			continue;
+		if (rect_short->y >= mission_description_rect.y + mission_description_rect.h - FontHeight(GetCurrentFont()))
+			continue;
+
+		if (stats_display[display])
+			ShowGenericButtonFromList(QUEST_BROWSER_ITEM_LONG_BUTTON);
+		else
+			ShowGenericButtonFromList(QUEST_BROWSER_ITEM_SHORT_BUTTON);
+
+		// This toggles the button (and displayed info):
+		if (MouseLeftPressed()) {
+			if (MouseCursorIsOnButton(QUEST_BROWSER_ITEM_SHORT_BUTTON, GetMousePos_x(), GetMousePos_y())) {
+				stats_display[display] = !stats_display[display];
+				while (MouseLeftPressed()) ;
+			}
+		}
+	}
+	free_autostr(stats_buffer);
+}
+
 /**
  *
  *
@@ -249,6 +433,8 @@ void quest_browser_display_mission_list(int list_type)
 				}
 			}
 		}
+	} else if (list_type == QUEST_BROWSER_SHOW_NOTES) {
+		print_statistics();
 	} else {
 		const char *txt;
 		
@@ -258,9 +444,6 @@ void quest_browser_display_mission_list(int list_type)
 				break;
 			case QUEST_BROWSER_SHOW_DONE_MISSIONS:
 				txt = _("No completed quests yet.");
-				break;
-			case QUEST_BROWSER_SHOW_NOTES:
-				txt = _("Greetings Valued Customer.\n\nThe Parafunken QuickNotes license expired 76 years ago.\nPlease update your subscription to continue using this software.\n\nThank you.");
 				break;
 			default:
 				txt = "Unknown quest browser status ?!?!?!"; 
