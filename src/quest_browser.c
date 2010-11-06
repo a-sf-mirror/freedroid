86c9f9aa89f0a1282efb7a9e833ea51682639473
ahuillet <ahuillet@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2010-11-06 11:40:56 +0000

    Use struct auto_string for quest browser.
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@3387 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/quest_browser.c
+++ src/quest_browser.c
@@ -34,22 +34,18 @@
 #include "global.h"
 #include "proto.h"
 
-int currently_selected_mission = (-1);
-SDL_Rect mission_list_rect = { 20, 280, 280, 180 };
-SDL_Rect mission_description_rect = { 134, 86, 280, 320 };
+static SDL_Rect mission_description_rect = { 134, 86, 280, 320 };
 
-char complete_mission_display_text[50000];
-float mission_list_offset = 0;
-int mission_list_scroll_override_from_user = 0;
+static struct auto_string *quest_browser_text;
+
+static int mission_list_scroll_override_from_user;
 static char stats_display[NB_DROID_TYPES];
 
 #define QUEST_BROWSER_SHOW_OPEN_MISSIONS (-1011)
 #define QUEST_BROWSER_SHOW_DONE_MISSIONS (-1012)
 #define QUEST_BROWSER_SHOW_NOTES (-1013)
 
-int current_quest_browser_mode = QUEST_BROWSER_SHOW_OPEN_MISSIONS;
-
-int quest_browser_mission_lines_needed[MAX_MISSIONS_IN_GAME];
+static int current_quest_browser_mode = QUEST_BROWSER_SHOW_OPEN_MISSIONS;
 
 /**
  * If there is some mission selected inside the quest browser, then we
@@ -59,51 +55,37 @@ int quest_browser_mission_lines_needed[MAX_MISSIONS_IN_GAME];
  */
 static void quest_browser_append_mission_info(const char *mis_name, int full_description)
 {
-	char temp_text[10000];
-	int mission_diary_index;
+	int i;
 	int mis_num = GetMissionIndexByName(mis_name);
+	struct mission *mis = &Me.AllMissions[mis_num];
 
-	strcat(complete_mission_display_text, ("\3"));
-	strcat(complete_mission_display_text, _("Quest:"));
-	strcat(complete_mission_display_text, (" \1"));
-	strcat(complete_mission_display_text, _(Me.AllMissions[mis_num].mission_name));
-	strcat(complete_mission_display_text, "\n\3");
-
-	// Depending on whether we want the full mission description or only
-	// some closed file description, we can either return or must continue
-	// to the detailed description.
-	//
+	autostr_append(quest_browser_text, "\3%s \1%s\n\3", _("Quest:"),  _(mis->mission_name));
+	
 	if (!full_description)
 		return;
 
-	strcat(complete_mission_display_text, ("\3"));
-	strcat(complete_mission_display_text, _("Status:"));
-	strcat(complete_mission_display_text, (" \2"));
-	if (Me.AllMissions[mis_num].MissionIsComplete)
-		strcat(complete_mission_display_text, _("COMPLETE"));
-	else if (Me.AllMissions[mis_num].MissionWasFailed)
-		strcat(complete_mission_display_text, _("FAILED"));
-	else
-		strcat(complete_mission_display_text, _("STILL OPEN"));
-	strcat(complete_mission_display_text, ("\n\3"));
-	strcat(complete_mission_display_text, _("Details:"));
-	strcat(complete_mission_display_text, ("\n"));
+	autostr_append(quest_browser_text, "\3%s \2", _("Status:"));
 
-	for (mission_diary_index = 0; mission_diary_index < MAX_MISSION_DESCRIPTION_TEXTS; mission_diary_index++) {
-		if (Me.AllMissions[mis_num].mission_diary_texts[mission_diary_index]) {
-			sprintf(temp_text, _("Day %d  %02d:%02d"),
-				get_days_of_game_duration(Me.AllMissions[mis_num].mission_description_time[mission_diary_index]),
-				get_hours_of_game_duration(Me.AllMissions[mis_num].mission_description_time[mission_diary_index]),
-				get_minutes_of_game_duration(Me.AllMissions[mis_num].mission_description_time[mission_diary_index]));
-			strcat(complete_mission_display_text, "\3-------- \1");
-			strcat(complete_mission_display_text, temp_text);
-			strcat(complete_mission_display_text, "\3 --------\n\2");
-			strcat(complete_mission_display_text, D_(Me.AllMissions[mis_num].mission_diary_texts[mission_diary_index]));
-			strcat(complete_mission_display_text, "\n\3");
+	const char *status;
+	if (mis->MissionIsComplete)
+		status = _("COMPLETE");
+	else if (mis->MissionWasFailed)
+		status = _("FAILED");
+	else
+		status = _("STILL OPEN");
+
+	autostr_append(quest_browser_text, "%s\n\3%s\n", status, _("Details:"));
+
+	for (i = 0; i < MAX_MISSION_DESCRIPTION_TEXTS; i++) {
+		if (mis->mission_diary_texts[i]) {
+			autostr_append(quest_browser_text, "\3-------- \1%s %d  %02d:%02d\3 --------\n\2%s\n\3", _("Day"), 
+					get_days_of_game_duration(mis->mission_description_time[i]),
+					get_hours_of_game_duration(mis->mission_description_time[i]),
+					get_minutes_of_game_duration(mis->mission_description_time[i]),
+					D_(mis->mission_diary_texts[i]));
 		}
 	}
-
-};				// void quest_browser_append_mission_info ( int mis_num )
+}
 
 //comparison function for droid names
 static int cmp_droid_names(const void *ptr1, const void *ptr2){
@@ -115,7 +97,6 @@ static int cmp_droid_names(const void *ptr1, const void *ptr2){
 //function that assembles and prints statistics about the player
 static void print_statistics(void)
 {
-	struct auto_string *stats_buffer = alloc_autostr(5000);
 	int statistics_browser_lines_needed[Number_Of_Droid_Types + 2];
 	int display = 0;                //display location
 	int srt[Number_Of_Droid_Types]; //used for alphabetizing droid names in display
@@ -154,16 +135,16 @@ static void print_statistics(void)
 	}
 
 	//Assemble Output
-	autostr_append(stats_buffer, _("\1Parafunken Statistics\2\n"));
+	autostr_printf(quest_browser_text, _("\1Parafunken Statistics\2\n"));
 
 	// This is where we display grand total statistics
-	autostr_append(stats_buffer, _("\1Overview\2\n"));
-	statistics_browser_lines_needed[display] = get_lines_needed(stats_buffer->value, mission_description_rect, 1);
+	autostr_append(quest_browser_text, _("\1Overview\2\n"));
+	statistics_browser_lines_needed[display] = get_lines_needed(quest_browser_text->value, mission_description_rect, 1);
 	if (stats_display[display++]) {
-		autostr_append(stats_buffer, _("Distance Traveled: \3%.1fm\2\n"), Me.meters_traveled);
-		autostr_append(stats_buffer, _("Destroyed Enemies: \3%i\2\n"), total_destroyed);
-		autostr_append(stats_buffer, _("Captured Enemies: \3%i\2\n"), total_takeover_success);
-		autostr_append(stats_buffer, _("Damage Dealt: \3%i\2\n"), total_damage_dealt);
+		autostr_append(quest_browser_text, _("Distance Traveled: \3%.1fm\2\n"), Me.meters_traveled);
+		autostr_append(quest_browser_text, _("Destroyed Enemies: \3%i\2\n"), total_destroyed);
+		autostr_append(quest_browser_text, _("Captured Enemies: \3%i\2\n"), total_takeover_success);
+		autostr_append(quest_browser_text, _("Damage Dealt: \3%i\2\n"), total_damage_dealt);
 	}
 
 	// This is where we display statistics about the best and worst relationships with bots:
@@ -171,34 +152,34 @@ static void print_statistics(void)
 	// Takeover  -> type with highest takeover % (%rate, # attempts)
 	// Defending -> type most damage done to Tux (# damage)
 	// Failed Takeovers -> lowest takeover % (% rate, # attempts)
-	autostr_append(stats_buffer, _("\1Preferences\2\n"));
-	statistics_browser_lines_needed[display] = get_lines_needed(stats_buffer->value, mission_description_rect, 1);
+	autostr_append(quest_browser_text, _("\1Preferences\2\n"));
+	statistics_browser_lines_needed[display] = get_lines_needed(quest_browser_text->value, mission_description_rect, 1);
 	if (stats_display[display++]) {
 		if (fav_destroyed != -1) {
-			autostr_append(stats_buffer, _("Attacking: \3%s\2 (\3%i\2 "), 
+			autostr_append(quest_browser_text, _("Attacking: \3%s\2 (\3%i\2 "), 
 				Druidmap[fav_destroyed].default_short_description, Me.destroyed_bots[fav_destroyed]);
 			if (Druidmap[fav_destroyed].is_human)
-				autostr_append(stats_buffer, _("killed,"));
+				autostr_append(quest_browser_text, _("killed,"));
 			else
-				autostr_append(stats_buffer, _("destroyed,"));
-			autostr_append(stats_buffer, _(" \3%i\2 DP)\n"), Me.damage_dealt[fav_destroyed]);
+				autostr_append(quest_browser_text, _("destroyed,"));
+			autostr_append(quest_browser_text, _(" \3%i\2 DP)\n"), Me.damage_dealt[fav_destroyed]);
 		} else
-			autostr_append(stats_buffer, _("Attacking: no droids destroyed\n"));
+			autostr_append(quest_browser_text, _("Attacking: no droids destroyed\n"));
 
 		if (fav_capture_target != -1)
-			autostr_append(stats_buffer, _("Takeover: \3%s\2 (\3%i%%\2 rate, \3%i\2 attempts)\n"),
+			autostr_append(quest_browser_text, _("Takeover: \3%s\2 (\3%i%%\2 rate, \3%i\2 attempts)\n"),
 					Druidmap[fav_capture_target].default_short_description, fav_capture_success_rate,
 					Me.TakeoverSuccesses[fav_capture_target] + Me.TakeoverFailures[fav_capture_target]);
 		else
-			autostr_append(stats_buffer, _("Takeover: none successful\n"));
+			autostr_append(quest_browser_text, _("Takeover: none successful\n"));
 
 
 		if (worst_capture_target != -1)
-			autostr_append(stats_buffer, _("Failed Takeover: \3%s\2 (\3%i%%\2 rate, \3%i\2 attempts)\n"),
+			autostr_append(quest_browser_text, _("Failed Takeover: \3%s\2 (\3%i%%\2 rate, \3%i\2 attempts)\n"),
 					Druidmap[worst_capture_target].default_short_description, worst_capture_success_rate,
 					Me.TakeoverSuccesses[worst_capture_target] + Me.TakeoverFailures[worst_capture_target]);
 		else
-			autostr_append(stats_buffer, _("Failed Takeover: no failures\n"));
+			autostr_append(quest_browser_text, _("Failed Takeover: no failures\n"));
 	}
 
 	//This sorts the droid types by default model name (from freedroid.droid_archetypes):
@@ -214,22 +195,22 @@ static void print_statistics(void)
 		model = srt[counter];
 		//only show bot types the player has interacted with:
 		if (Me.damage_dealt[model] || Me.TakeoverSuccesses[model] || Me.TakeoverFailures[model] || Me.destroyed_bots[model]) {
-			autostr_append(stats_buffer, "\1%s\2\n", Druidmap[model].default_short_description);
-			statistics_browser_lines_needed[display] = get_lines_needed(stats_buffer->value, mission_description_rect, 1);
+			autostr_append(quest_browser_text, "\1%s\2\n", Druidmap[model].default_short_description);
+			statistics_browser_lines_needed[display] = get_lines_needed(quest_browser_text->value, mission_description_rect, 1);
 			if (stats_display[display++]) {
 				if (Druidmap[model].is_human) {
-					autostr_append(stats_buffer, _("Killed: \3%d\2\n"), Me.destroyed_bots[model]);
+					autostr_append(quest_browser_text, _("Killed: \3%d\2\n"), Me.destroyed_bots[model]);
 				} else {
-					autostr_append(stats_buffer, _("Destroyed: \3%d\2\n"), Me.destroyed_bots[model]);
-					autostr_append(stats_buffer, _("Takeovers: \3%d\2/\3%d\2/\3"), Me.TakeoverSuccesses[model], Me.TakeoverFailures[model]);
+					autostr_append(quest_browser_text, _("Destroyed: \3%d\2\n"), Me.destroyed_bots[model]);
+					autostr_append(quest_browser_text, _("Takeovers: \3%d\2/\3%d\2/\3"), Me.TakeoverSuccesses[model], Me.TakeoverFailures[model]);
 					if (Me.TakeoverSuccesses[model] + Me.TakeoverFailures[model])
-						autostr_append(stats_buffer, _("%d%%\2"),
+						autostr_append(quest_browser_text, _("%d%%\2"),
 							(100 * Me.TakeoverSuccesses[model])/(Me.TakeoverSuccesses[model] + Me.TakeoverFailures[model]));
 					else
-						autostr_append(stats_buffer, _(" 0%%\2"));
-					autostr_append(stats_buffer, _(" (success/fail/ratio)\n"));
+						autostr_append(quest_browser_text, _(" 0%%\2"));
+					autostr_append(quest_browser_text, _(" (success/fail/ratio)\n"));
 				}
-				autostr_append(stats_buffer, _("Damage: \3%d\2 (dealt)\n"), Me.damage_dealt[model]);
+				autostr_append(quest_browser_text, _("Damage: \3%d\2 (dealt)\n"), Me.damage_dealt[model]);
 			}
 		} else {
 			statistics_browser_lines_needed[display++] = -1;
@@ -237,9 +218,9 @@ static void print_statistics(void)
 	}
 
 	//Print all to screen
-	mission_list_offset = (FontHeight(GetCurrentFont()) * TEXT_STRETCH)
-	    * (mission_list_scroll_override_from_user) * 1.00;
-	DisplayText(stats_buffer->value, mission_description_rect.x,
+	float mission_list_offset = (FontHeight(GetCurrentFont()) * TEXT_STRETCH)
+	    * mission_list_scroll_override_from_user;
+	DisplayText(quest_browser_text->value, mission_description_rect.x,
 			    mission_description_rect.y - mission_list_offset, &mission_description_rect, 1);
 
 	// Now it's time to display some short/long symbols in front
@@ -286,7 +267,6 @@ static void print_statistics(void)
 			}
 		}
 	}
-	free_autostr(stats_buffer);
 }
 
 /**
@@ -297,20 +277,16 @@ static void quest_browser_display_mission_list(int list_type)
 {
 	int mis_num;
 	int something_was_displayed = FALSE;
+	int quest_browser_mission_lines_needed[MAX_MISSIONS_IN_GAME];
 
-	strcpy(complete_mission_display_text, "");
+	autostr_printf(quest_browser_text, "");
 
 	for (mis_num = 0; mis_num < MAX_MISSIONS_IN_GAME; mis_num++) {
-		// The mission short/long symbol positions must be initialized
-		// with some default values.
-		//
-		quest_browser_mission_lines_needed[mis_num] = (-1);
+		quest_browser_mission_lines_needed[mis_num] = -1;
 
-		// In case the mission does not exist at all, we need not do anything more...
 		if (Me.AllMissions[mis_num].MissionExistsAtAll != TRUE)
 			continue;
 
-		//  In case the mission was not yet assigned, we need not do anything more...
 		if (Me.AllMissions[mis_num].MissionWasAssigned != TRUE)
 			continue;
 
@@ -318,12 +294,8 @@ static void quest_browser_display_mission_list(int list_type)
 		// draw suitable long/short buttons in front of the text, so that the
 		// user can then select to see the full/short information on this quest.
 		//
-		if(strlen(complete_mission_display_text) == 0) {
-			quest_browser_mission_lines_needed[mis_num] = 0;
-		} else {
-			quest_browser_mission_lines_needed[mis_num] =
-				get_lines_needed(complete_mission_display_text, mission_description_rect, TEXT_STRETCH);
-		}
+		quest_browser_mission_lines_needed[mis_num] =
+			get_lines_needed(quest_browser_text->value, mission_description_rect, TEXT_STRETCH);
 
 		if ((list_type == QUEST_BROWSER_SHOW_OPEN_MISSIONS) && (Me.AllMissions[mis_num].MissionIsComplete == FALSE)) {
 			quest_browser_append_mission_info(Me.AllMissions[mis_num].mission_name,
@@ -334,15 +306,15 @@ static void quest_browser_display_mission_list(int list_type)
 							  Me.AllMissions[mis_num].expanded_display_for_this_mission);
 			something_was_displayed = TRUE;
 		} else {
-			quest_browser_mission_lines_needed[mis_num] = (-1);
+			quest_browser_mission_lines_needed[mis_num] = -1;
 		}
 
 	}
 
 	if (something_was_displayed) {
-		mission_list_offset = (FontHeight(GetCurrentFont()) * TEXT_STRETCH)
-		    * (mission_list_scroll_override_from_user) * 1.00;
-		DisplayText(complete_mission_display_text, mission_description_rect.x,
+		float mission_list_offset = (FontHeight(GetCurrentFont()) * TEXT_STRETCH)
+		    * mission_list_scroll_override_from_user;
+		DisplayText(quest_browser_text->value, mission_description_rect.x,
 			    mission_description_rect.y - mission_list_offset, &mission_description_rect, TEXT_STRETCH);
 
 		// Now it's time to display some short/long symbols in front
@@ -362,7 +334,7 @@ static void quest_browser_display_mission_list(int list_type)
 			// problem, because we won't blit the thing in this case
 			// anyway.
 			//
-			if (quest_browser_mission_lines_needed[mis_num] != (-1)) {
+			if (quest_browser_mission_lines_needed[mis_num] != -1) {
 				rect_short->y =
 				    mission_description_rect.y - mission_list_offset +
 				    (FontHeight(GetCurrentFont()) * TEXT_STRETCH) * (quest_browser_mission_lines_needed[mis_num]) - 2;
@@ -377,7 +349,7 @@ static void quest_browser_display_mission_list(int list_type)
 			//
 			if (rect_short->y <= mission_description_rect.y - 4)
 				continue;
-			if (rect_short->y >= mission_description_rect.y + mission_description_rect.h - FontHeight(GetCurrentFont()) * TEXT_STRETCH)
+			if (rect_short->y >= mission_description_rect.y + mission_description_rect.h)
 				continue;
 
 			if (Me.AllMissions[mis_num].expanded_display_for_this_mission)
@@ -393,8 +365,6 @@ static void quest_browser_display_mission_list(int list_type)
 				}
 			}
 		}
-	} else if (list_type == QUEST_BROWSER_SHOW_NOTES) {
-		print_statistics();
 	} else {
 		const char *txt;
 		
@@ -404,9 +374,6 @@ static void quest_browser_display_mission_list(int list_type)
 				break;
 			case QUEST_BROWSER_SHOW_DONE_MISSIONS:
 				txt = _("No completed quests yet.");
-				break;
-			default:
-				txt = "Unknown quest browser status ?!?!?!"; 
 				break;
 		}
 
@@ -437,6 +404,7 @@ void quest_browser_interface(void)
 		mission_description_rect.y *= (((float)GameConfig.screen_height) / 480.0);
 		mission_description_rect.w *= (((float)GameConfig.screen_width) / 640.0);
 		mission_description_rect.h *= (((float)GameConfig.screen_height) / 480.0);
+		quest_browser_text = alloc_autostr(1000);
 	}
 	// This might take some time, so we need to be careful here,
 	// so as not to generate a massive frame time, that would
