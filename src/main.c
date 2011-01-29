ac8ee5a6573d28e91208dc4a0b08ed35cbe69a30
matthiaskrgr <matthiaskrgr@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2011-01-29 21:33:35 +0000

    Configuration: Don't run the game in fullscreen mode by default.
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@3818 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/main.c
+++ src/main.c
@@ -165,7 +165,7 @@ better than nothing.  Thanks anyway for your interest in FreedroidRPG.\n\
 	use_open_gl = TRUE;	// default value, can be overridden by command-line 
 	start_editor = FALSE;	// default value, can be overridden by command-line 
 	debug_level = -1;	// -1: shut up all debug ... 0=no debug 1=first debug level (at the moment=all) 
-	GameConfig.fullscreen_on = TRUE;	// use X11-window or full screen 
+	GameConfig.fullscreen_on = FALSE;	// use X11-window or full screen
 
 	InitFreedroid(argc, argv);	// Initialisation of global variables and arrays
 
