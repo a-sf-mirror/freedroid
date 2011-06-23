a-sf-mirror/freedroid

commit 338c2cf783497f66e3fbaa56fdd28f3f9bc44f9b

lvledit: Only draw map grid on the visible parts of the level.
git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@4729 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

src/view.c
@@ -1725,14 +1725,24 @@ void draw_grid_on_the_floor(int mask)
 	float dd;
 
 	if (GameConfig.show_grid >= 2) {	// large grid
-		// Draw the horizontal lines
-		for (dd = 0; dd <= our_level->ylen; dd++) {
-			draw_line_on_map(0, dd, our_level->xlen, dd, 0x99FFFF, 1);	// light cyan
- 		}
 
-		// Draw the vertical lines
-		for (dd = 0; dd <= our_level->xlen; dd++) {
-			draw_line_on_map(dd, 0, dd, our_level->ylen, 0x99FFFF, 1);	// light cyan
+		if (LineStart < 0)
+			LineStart = 0;
+		if (LineEnd > our_level->ylen)
+			LineEnd = our_level->ylen;
+		if (ColStart < 0)
+			ColStart = 0;
+		if (ColEnd > our_level->xlen)
+			ColEnd = our_level->xlen;
+
+		// Draw horizontal lines.
+		for (dd = LineStart; dd <= LineEnd; dd++) {
+			draw_line_on_map(LineStart, dd, LineEnd, dd, 0x99FFFF, 1);	// light cyan
+		}
+
+		// Draw vertical lines.
+		for (dd = ColStart; dd <= ColEnd; dd++) {
+			draw_line_on_map(dd, ColStart, dd, ColEnd, 0x99FFFF, 1);	// light cyan
 		}
 	}
 

commit f3e617b1c3c7c55eb155b24ba35af3599120257e

lvledit: Fix map grid not drawn correctly. Fix a bug introduced in r4729.
git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@4751 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

src/view.c
@@ -1737,12 +1737,12 @@ void draw_grid_on_the_floor(int mask)
 
 		// Draw horizontal lines.
 		for (dd = LineStart; dd <= LineEnd; dd++) {
-			draw_line_on_map(LineStart, dd, LineEnd, dd, 0x99FFFF, 1);	// light cyan
+			draw_line_on_map(ColStart, dd, ColEnd, dd, 0x99FFFF, 1);	// light cyan
 		}
 
 		// Draw vertical lines.
 		for (dd = ColStart; dd <= ColEnd; dd++) {
-			draw_line_on_map(dd, ColStart, dd, ColEnd, 0x99FFFF, 1);	// light cyan
+			draw_line_on_map(dd, LineStart, dd, LineEnd, 0x99FFFF, 1);	// light cyan
 		}
 	}
 