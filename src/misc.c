3f7e711641f07f194176b0dd7d9145c200d14357
hakzsam <hakzsam@524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61>
2011-07-01 12:10:56 +0000

    lvledit: Fix a little offset in resolution 640x480 by moving the toggle waypoint connections button. Patch by Matei Pavaluca.
    git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@4782 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

--- src/misc.c
+++ src/misc.c
@@ -134,13 +134,13 @@ mouse_press_button AllMousePressButtons[MAX_MOUSE_PRESS_BUTTONS] = {
 
 
 	[LEVEL_EDITOR_TOGGLE_WAYPOINT_CONNECTIONS_BUTTON] =
-		{EMPTY_IMAGE, "mouse_buttons/LevelEditorToggleWaypointConnectionsButton.png", {-30, -380, 0, 0}, FALSE},
+		{EMPTY_IMAGE, "mouse_buttons/LevelEditorToggleWaypointConnectionsButton.png", {-90, 80, 0, 0}, FALSE},
 	[LEVEL_EDITOR_TOGGLE_WAYPOINT_CONNECTIONS_BUTTON_PUSHED] =
-		{EMPTY_IMAGE, "mouse_buttons/LevelEditorToggleWaypointConnectionsButtonPushed.png", {-29, -380, 0, 0}, FALSE},
+		{EMPTY_IMAGE, "mouse_buttons/LevelEditorToggleWaypointConnectionsButtonPushed.png", {-89, 79, 0, 0}, FALSE},
 	[LEVEL_EDITOR_TOGGLE_WAYPOINT_CONNECTIONS_BUTTON_OFF] =
-		{EMPTY_IMAGE, "mouse_buttons/LevelEditorToggleWaypointConnectionsButtonOff.png", {-30, -380, 0, 0}, FALSE},
+		{EMPTY_IMAGE, "mouse_buttons/LevelEditorToggleWaypointConnectionsButtonOff.png", {-90, 80, 0, 0}, FALSE},
 	[LEVEL_EDITOR_TOGGLE_WAYPOINT_CONNECTIONS_BUTTON_OFF_PUSHED] =
-		{EMPTY_IMAGE, "mouse_buttons/LevelEditorToggleWaypointConnectionsButtonOffPushed.png", {-29, -380, 0, 0}, FALSE},
+		{EMPTY_IMAGE, "mouse_buttons/LevelEditorToggleWaypointConnectionsButtonOffPushed.png", {-89, 79, 0, 0}, FALSE},
 
 
 /* above the obstacle selectors, upper row */
