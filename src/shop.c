a-sf-mirror/freedroid

commit 4e197310363398cdf40b717b5f03fe25bcd8d543

shop: Prevent selecting past the last item (fixes #3050304).
git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@4796 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

src/shop.c
@@ -553,14 +553,14 @@ int GreatShopInterface(int NumberOfItems, item * ShowPointerList[MAX_ITEMS_IN_IN
 				}
 				MoveMenuPositionSound();
 			} else if (((ClickTarget = ClickWasOntoItemRowPosition(GetMousePos_x(), GetMousePos_y(), FALSE)) >= 0)) {
-				if (RowStart + ClickTarget < NumberOfItems) {
+				if (ClickTarget < RowLength) {
 					ItemIndex = RowStart + ClickTarget;
 					TuxItemIndex = (-1);
 					fill_item_description(&item_description, ShowPointerList[ItemIndex], 1);
 					item_description.scroll_offset = scroll_to_top;
 				}
 			} else if (((ClickTarget = ClickWasOntoItemRowPosition(GetMousePos_x(), GetMousePos_y(), TRUE)) >= 0)) {
-				if (TuxRowStart + ClickTarget < NumberOfItemsInTuxRow) {
+				if (ClickTarget < RowLength) {
 					TuxItemIndex = TuxRowStart + ClickTarget;
 					ItemIndex = (-1);
 					fill_item_description(&item_description, TuxItemsList[TuxItemIndex], 0);

commit 0c2484bad97766e351fb1bea7dd08bc9c13b639d

shop: Fix selecting for Tux's items. Fix a bug introduced in r4796.
git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@4831 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

src/shop.c
@@ -562,7 +562,7 @@ int GreatShopInterface(int NumberOfItems, item * ShowPointerList[MAX_ITEMS_IN_IN
 					item_description.scroll_offset = scroll_to_top;
 				}
 			} else if (((ClickTarget = ClickWasOntoItemRowPosition(GetMousePos_x(), GetMousePos_y(), TRUE)) >= 0)) {
-				if (ClickTarget < RowLength) {
+				if (ClickTarget < TuxRowLength) {
 					TuxItemIndex = TuxRowStart + ClickTarget;
 					ItemIndex = (-1);
 					fill_item_description(&item_description, TuxItemsList[TuxItemIndex], 0);