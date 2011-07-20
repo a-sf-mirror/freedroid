a-sf-mirror/freedroid

commit 3a6cd9e499e6cfb0a987f2932927456d6b63f10a

use new Range-of-Values parser in map.c. Patch by Miles.
git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@3985 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

src/map.c
@@ -1721,28 +1721,9 @@ void GetThisLevelsDroids(char *section_pointer)
 	ReadValueFromString(section_pointer, DROIDS_LEVEL_INDICATION_STRING, "%d", &our_level_number, lvl_end_location);
 
 	// Now we read in the min and max number of random droids for this level
-	search_ptr = ReadAndMallocStringFromDataOptional(section_pointer, DROIDS_NUMBER_INDICATION_STRING, "\n");
-	if (search_ptr) {
-		char *ptr;
-		min_rand = strtol(search_ptr, &ptr, 10);
-		if (*ptr == '-') {
-			ptr++;
-			max_rand = strtol(ptr, NULL, 10);
-		} else {
-			max_rand = min_rand;
-		}
-
-		if (min_rand < 0) {
-			ErrorMessage(__FUNCTION__, "\
-On Level %d the minimum number (%d) of random droids is a negative number.\n\
-Setting the number of random droids to 0 for this level.", PLEASE_INFORM, IS_WARNING_ONLY, our_level_number, min_rand);
-			min_rand = max_rand = 0;
-		} else if (max_rand < min_rand) {
-			ErrorMessage(__FUNCTION__, "\
-On Level %d the minimum number (%d) of random droids is greater than\n\
-the maximum number (%d) of random droids.", PLEASE_INFORM, IS_WARNING_ONLY, our_level_number, min_rand, max_rand);
-		} 
-		free(search_ptr);
+	if (ReadRangeFromString(section_pointer, DROIDS_NUMBER_INDICATION_STRING, "\n", &min_rand, &max_rand, 0) > 1) {
+		ErrorMessage(__FUNCTION__, "\
+The previous error was a garbled number of random droids defined on Level %d.", PLEASE_INFORM, IS_WARNING_ONLY, our_level_number);
 	}
 
 	// Now we read in the type(s) of random droids for this level

commit 46593e41dc2db1d2bc7518f243141dce2381978d

Fix swapped min and max values for the random droids. Bug introduced in r3985. Patch by Matei Pavaluca.
git-svn-id: https://freedroid.svn.sourceforge.net/svnroot/freedroid@4851 524c2bfb-fa3f-0410-b5a2-92f4e1dc5b61

src/map.c
@@ -1711,7 +1711,7 @@ void GetThisLevelsDroids(char *section_pointer)
 	ReadValueFromString(section_pointer, DROIDS_LEVEL_INDICATION_STRING, "%d", &our_level_number, lvl_end_location);
 
 	// Now we read in the min and max number of random droids for this level
-	if (ReadRangeFromString(section_pointer, DROIDS_NUMBER_INDICATION_STRING, "\n", &min_rand, &max_rand, 0) > 1) {
+	if (ReadRangeFromString(section_pointer, DROIDS_NUMBER_INDICATION_STRING, "\n", &max_rand, &min_rand, 0) > 1) {
 		ErrorMessage(__FUNCTION__, "\
 The previous error was a garbled number of random droids defined on Level %d.", PLEASE_INFORM, IS_WARNING_ONLY, our_level_number);
 	}