a-sf-mirror/freedroid

commit a49e3ba050e3728a80aeb8eadf2a7cd6f212c422

option menu / dialog: AfterTakeover : move "Reprogramm bots after Takeover" from settings menu to AfterTakeover settings dialog.

src/menu.c
@@ -1429,7 +1429,6 @@ static int Droid_handle(int n)
 	};
 	int *ptrs[] = {
 		&GameConfig.All_Texts_Switch,
-		&GameConfig.talk_to_bots_after_takeover,
 	};
 
 	if (n < LEAVE_DROID_TALK_OPTIONS_MENU && n > 0)
@@ -1447,11 +1446,6 @@ static void Droid_fill(char *MenuTexts[MAX_MENU_ITEMS])
 	int i = 0;
 	sprintf(Options[i], _("Show enemies' states"));
 	sprintf(Options[i + 1], ": %s", GameConfig.All_Texts_Switch ? _("YES") : _("NO"));
-	strcat(Options[i], Options[i + 1]);
-	strncpy(MenuTexts[i], Options[i], 1024);
-	i++;
-	sprintf(Options[i], _("Reprogram bots after takeover"));
-	sprintf(Options[i + 1], ": %s", GameConfig.talk_to_bots_after_takeover ? _("YES") : _("NO"));
 	strcat(Options[i], Options[i + 1]);
 	strncpy(MenuTexts[i], Options[i], 1024);
 	i++;

dialogs/AfterTakeover.dialog
@@ -27,6 +27,7 @@ freedroid-discussion@lists.sourceforge.net
 
 <FirstTime LuaCode>
 	Aftertakeover_drain_attempt = "ask"
+	Aftertakeover_reprogramm_bots_after_takeover = false
 </LuaCode>
 
 <EveryTime LuaCode>
@@ -333,7 +334,7 @@ NO_TEXT
 		show(35)
 	end
 	npc_says(_"Which setting should be changed?", "NO_WAIT")
-	hide(32, 33, 36, 37) show(31, 70, 80)
+	hide(32, 33, 36, 37) show(31, 70, 80, 74)
 </LuaCode>
 ----------------------------------------------------------------------
 
@@ -598,6 +599,43 @@ Topic="drain_bot_attempt_state"
 <LuaCode>
 	Aftertakeover_drain_attempt = "ask"
 	hide(71, 72, 73)
+	pop_topic()
+</LuaCode>
+----------------------------------------------------------------------
+
+Nr=74 Text=_"Reprogramm_bots_after_takeover\?.sh"
+Topic="settings.plx"
+<LuaCode>
+	if (Aftertakeover_reprogramm_bots_after_takeover) then
+		npc_says(_"Bots are reprogrammed after takeover: [b]true[/b]")
+	elseif not (Aftertakeover_reprogramm_bots_after_takeover) then
+		npc_says(_"Bots are reprogrammed after takeover: [b]false[/b]")
+	else -- ???
+		npc_says("ERROR   REPROGRAMM BOTS AFTER TAKEOVER, NODE 74  UNHANDLED VALUE OF  Aftertakeover_reprogramm_bots_after_takeover , PLEASE REPORT")
+	end
+	show(75, 76)
+	topic("reprogramm_bots")
+</LuaCode>
+----------------------------------------------------------------------
+
+Nr=75 Text="Yes"
+Topic="reprogramm_bots"
+<LuaCode>
+	npc_says("Bots will be reprogrammed after takeover.")
+	reprogramm_bots_after_takeover(1) -- 1 = true in C
+	Aftertakeover_reprogramm_bots_after_takeover = true
+	hide(75, 76)
+	pop_topic()
+</LuaCode>
+----------------------------------------------------------------------
+
+Nr=76 Text="No"
+Topic="reprogramm_bots"
+<LuaCode>
+	npc_says("Bots will not be reprogrammed after takeover.")
+	reprogramm_bots_after_takeover(0) -- 0 = false in C
+	Aftertakeover_reprogramm_bots_after_takeover = false
+	hide(75, 76)
 	pop_topic()
 </LuaCode>
 ----------------------------------------------------------------------

src/lua.c
@@ -1124,6 +1124,13 @@ static int lua_running_benchmark(lua_State *L)
 	return 1;
 }
 
+static int lua_reprogramm_bots_after_takeover(lua_State *L)
+{
+int rvat = luaL_checkinteger(L, 1);
+GameConfig.talk_to_bots_after_takeover = rvat;
+
+return 0;
+}
 
 static int lua_switch_background_music_to(lua_State *L)
 {
@@ -1410,7 +1417,8 @@ luaL_Reg lfuncs[] = {
 	 * play_sound("menu.ogg")
 	 */
 	{"switch_background_music", lua_switch_background_music_to},
-
+	// 1 = true,  0 = false
+	{"reprogramm_bots_after_takeover", lua_reprogramm_bots_after_takeover},
 
 
 	{NULL, NULL}

src/skills.c
@@ -367,7 +367,6 @@ int DoSkill(int skill_index, int SpellCost)
 				// go directly to chat to choose droid program
 				if (GameConfig.talk_to_bots_after_takeover)
 					chat_with_droid(droid_below_mouse_cursor);
-					
 				goto out;
 			}
 		} else {

commit 3ee612c4207700e75e1ad61f6c3f63ab9dee415b

menu: fix "Back" button in  Droid Talk  section of the options menu which was broken since a49e3ba050e3728a80aeb8eadf2a7cd6f212c422

src/menu.c
@@ -1424,7 +1424,6 @@ static int Droid_handle(int n)
 {
 	enum {
 		ALL_TEXTS = 1,
-		TALK_AFTER_TO,
 		LEAVE_DROID_TALK_OPTIONS_MENU
 	};
 	int *ptrs[] = {