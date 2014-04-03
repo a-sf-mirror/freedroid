a-sf-mirror/freedroid

commit 21cd4a4297d85e166e7464be369a9bd369d28a08

Compute the path of all data dirs (graphics, dialogs, sounds, ...) at initialization of the game. This avoid to check several top dirs during each file loading.
Also use PATH_MAX as the size of char buffers containing filenames
and pathnames.

src/defs.h
@@ -218,12 +218,6 @@ enum {
 #define FD_DATADIR ".."		// our local fallback
 #endif
 
-#define GRAPHICS_DIR  "graphics/"
-#define SOUND_DIR     "sound/"
-#define MAP_DIR       "map/"
-#define TITLES_DIR    "map/titles/"
-#define DIALOG_DIR    "dialogs/"
-#define LUA_MOD_DIR   "lua_modules/"
 #define ICON_FILE     "paraicon.bmp"
 
 //--------------------
@@ -1564,6 +1558,19 @@ enum chat_context_state {
 	SELECT_NEXT_NODE = 4,
 	RUN_NODE_SCRIPT = 5
 };
+
+// Indexes of known data dirs (used in conjunction with data_dirs[])
+enum data_dir_handle {
+	GRAPHICS_DIR = 0,
+	SOUND_DIR,
+	MAP_DIR,
+	TITLES_DIR,
+	DIALOG_DIR,
+	LUA_MOD_DIR,
+	LOCALE_DIR,
+	LAST_DATA_DIR
+};
+
 
 // Draw quads borders (OpenGL only)
 #define DEBUG_QUAD_BORDER 0

src/misc.c
@@ -61,6 +61,17 @@ char *our_homedir = NULL;
 char *our_config_dir = NULL;
 int term_has_color_cap = FALSE;
 
+struct data_dir data_dirs[] = {
+	[GRAPHICS_DIR]={ "graphics",    "" },
+	[SOUND_DIR]=   { "sound",       "" },
+	[MAP_DIR]=     { "map",         "" },
+	[TITLES_DIR]=  { "map/titles",  "" },
+	[DIALOG_DIR]=  { "dialogs",     "" },
+	[LUA_MOD_DIR]= { "lua_modules", "" },
+	[LOCALE_DIR]=  { "locale",      "" }
+};
+#define WELL_KNOWN_DATA_FILE "lua_modules/FDdialog.lua"
+
 mouse_press_button AllMousePressButtons[MAX_MOUSE_PRESS_BUTTONS] = {
 	[UP_BUTTON] = {EMPTY_IMAGE, "mouse_buttons/UpButton.png", {600, 94, 40, 40}, TRUE},
 	[DOWN_BUTTON] = {EMPTY_IMAGE, "mouse_buttons/DownButton.png", {600, 316, 40, 40}, TRUE},
@@ -606,149 +617,86 @@ void ShowGenericButtonFromList(int ButtonIndex)
 	display_image_on_screen(img, AllMousePressButtons[ButtonIndex].button_rect.x * scale_x, AllMousePressButtons[ButtonIndex].button_rect.y * scale_y, set_image_transformation(scale_x, scale_y, 1.0, 1.0, 1.0, 1.0, 0));
 }
 
-/* -----------------------------------------------------------------
- * find a given filename in subdir relative to FD_DATADIR, 
- *
- * if you pass NULL as subdir, it will be ignored
- *
- * fills in the (ALLOC'd) string and returns 0 if okay, 1 on error
- *
- * ----------------------------------------------------------------- */
-int find_file(const char *fname, const char *subdir, char *File_Path, int silent)
+int init_data_dirs_path()
 {
-	int i;
-	FILE *fp;		// this is the file we want to find?
+	int i, j;
+	FILE *f;
+	char file_path[PATH_MAX];
 
-	*File_Path = 0;
-	if (!subdir)
-		subdir = "";
-
-	for (i = 0; i < 4; i++) {
-		if (i == 0)
-			sprintf((File_Path), ".");	/* first try local subdirs */
-		if (i == 1)
-			sprintf((File_Path), "..");	/* first try local subdirs */
-		if (i == 2)
-			sprintf((File_Path), "../..");	/* first try local subdirs */
-		if (i == 3)
-			sprintf((File_Path), "%s", FD_DATADIR);	/* then the DATADIR */
-
-		strcat((File_Path), "/");
-		strcat((File_Path), subdir);
-		strcat((File_Path), "/");
-
-		strcat((File_Path), fname);
-
-		if ((fp = fopen((File_Path), "r")) != NULL) {	/* found it? */
-			fclose(fp);
-			break;
-		} else {
-			if (i != 3)
-				DebugPrintf(1, "\nfind_file could not succeed with LOCAL path: %s.", File_Path);
-			else {
-				if (!silent) {
-					DebugPrintf(-4, "The file name was: %s.\n", fname);
-					ErrorMessage(__FUNCTION__, "File not found ", NO_NEED_TO_INFORM, IS_WARNING_ONLY);
-				}
-				return 1;
-			}
-		}
-	}			// for i 
-
-	// DebugPrintf( 0 , "\nfind_file determined file path: %s." , File_Path );
-
-	return 0;
-};				// char * find_file ( ... )
-
-int find_subdir(const char *subdir, char *subdir_path)
-{
-	int i;
-	DIR *dir_stream = NULL;
-
-	if (!subdir) {
-		ErrorMessage(__FUNCTION__, "Subdir parameter is empty.", NO_NEED_TO_INFORM, IS_WARNING_ONLY);
-		return 1;
+	// Reset the data dirs paths
+	for (i = 0; i < LAST_DATA_DIR; i++) {
+		data_dirs[i].path[0] = '\0';
 	}
 
-	*subdir_path = '\0';
+	// Directories to look for the data dirs
+	char *top_data_dir[]   = { ".", "..", "../..", FD_DATADIR };
+	char *top_locale_dir[] = { ".", "..", "../..", LOCALEDIR };
+	int slen = sizeof(top_data_dir)/sizeof(top_data_dir[0]);
 
-	for (i = 0; i < 4; i++) {
-		if (i == 0)
-			sprintf(subdir_path, ".");	/* first try local subdirs */
-		if (i == 1)
-			sprintf(subdir_path, "..");	/* first try local subdirs */
-		if (i == 2)
-			sprintf(subdir_path, "../..");	/* first try local subdirs */
-		if (i == 3)
-			sprintf(subdir_path, FD_DATADIR);	/* then the DATADIR */
+	// To find the root of the data dirs, we search a well known file that is
+	// always needed for the game to work.
+	for (i = 0; i < slen ; i++) {
+		sprintf(file_path, "%s/" WELL_KNOWN_DATA_FILE, top_data_dir[i]);
 
-		strcat(subdir_path, "/");
-		strcat(subdir_path, subdir);
-
-		if ((dir_stream = opendir(subdir_path)) != NULL) {	/* found it? */
-			closedir(dir_stream);
-			break;
-		} else {
-			if (i == 3) {
-				ErrorMessage(__FUNCTION__, "Subdir %s not found.\n", NO_NEED_TO_INFORM, IS_WARNING_ONLY, subdir);
-				return 1;
+		if ((f = fopen(file_path, "r")) != NULL) {
+			// File found, so now fill the data dir paths
+			for (j = 0; j < LAST_DATA_DIR; j++) {
+				char *dir = (j == LOCALE_DIR) ? top_locale_dir[i] : top_data_dir[i];
+				int nb = snprintf(data_dirs[j].path, PATH_MAX, "%s/%s", dir, data_dirs[j].name);
+				if (nb >= PATH_MAX) {
+					ErrorMessage(__FUNCTION__, "data_dirs[].path is not big enough to store the following path: %s/%s",
+					             PLEASE_INFORM, IS_FATAL, dir, data_dirs[j].name);
+				}
 			}
+			fclose(f);
+			return 1;
 		}
+	}
+
+	// The searched file was not found. Complain.
+	if (getcwd(file_path, PATH_MAX)) {
+		ErrorMessage(__FUNCTION__, "Data dirs not found ! (current directory: %s)",
+	                 PLEASE_INFORM, IS_FATAL, file_path);
+	} else {
+		ErrorMessage(__FUNCTION__, "Data dirs not found ! (and cannot find the current working directory)",
+	                 PLEASE_INFORM, IS_FATAL);
 	}
 
 	return 0;
 }
 
-const char *find_localedir(void)
+/* -----------------------------------------------------------------
+ * find a given filename in subdir.
+ *
+ * fills in the (ALLOC'd) string and returns 0 if okay, 1 on error.
+ * file_path's length HAS to be PATH_MAX.
+ * ----------------------------------------------------------------- */
+int find_file(const char *fname, int subdir_handle, char *file_path)
 {
-#ifdef ENABLE_NLS
-	// We want to choose between the use of a local subdir containing
-	// the translations or the system wide subdir. It depends on how
-	// the game is started and if we use an installed version or a built
-	// one.
-	// There is however no easy way to check if a folder contains a gettext
-	// locale infrastructure.
-	// So, we have to use a trick: we search a mandatory file (levels.dat) and
-	// depending on where it is found we decide which folder to use as the
-	// locale subdir.
-
-	int i;
-	char *file_path;
 	FILE *fp;
 
-	for (i = 0; i < 3; i++) {
-		if (i == 0)
-			file_path = "./" MAP_DIR "levels.dat";
-		if (i == 1)
-			file_path = "../" MAP_DIR "levels.dat";
-		if (i == 2)
-			file_path = "../.." MAP_DIR "levels.dat";
-
-		if ((fp = fopen((file_path), "r")) != NULL) {	/* found it? */
-			fclose(fp);
-			break;
-		}
+	if (subdir_handle < 0 || subdir_handle >= LAST_DATA_DIR) {
+		ErrorMessage(__FUNCTION__, "Called with a wrong subdir handle (%d)",
+		             NO_NEED_TO_INFORM, IS_WARNING_ONLY, subdir_handle);
+		return 1;
 	}
 
-	switch (i) {
-	case 0:
-		return "./locale";
-		break;
-	case 1:
-		return "../locale";
-		break;
-	case 2:
-		return "../../locale";
-		break;
-	default:
-		return LOCALEDIR;
-		break;
+	int nb = snprintf(file_path, PATH_MAX, "%s/%s", data_dirs[subdir_handle].path, fname);
+	if (nb >= PATH_MAX) {
+		*file_path = 0;
+		ErrorMessage(__FUNCTION__, "Pathname too long (max is %d): %s/%s",
+		             NO_NEED_TO_INFORM, IS_WARNING_ONLY, PATH_MAX, data_dirs[subdir_handle].name, fname);
+		return 1;
 	}
 
-	return LOCALEDIR;
-#else
-	return NULL;
-#endif
+	if ((fp = fopen(file_path, "r")) == NULL) {	/* not found */
+		*file_path = 0;
+		ErrorMessage(__FUNCTION__, "File %s not found in %s/",
+		             NO_NEED_TO_INFORM, IS_WARNING_ONLY, fname, data_dirs[subdir_handle].name);
+		return 1;
+	}
+
+	return 0;
 }
 
 /**

lua_modules/FDdialog.lua
@@ -406,15 +406,15 @@ end
 ------------
 
 --! \privatesection
-FDdialog.dialogs_dir = "../dialogs"     --!< \brief Directory containing the dialogs     \memberof Lua::FDdialog
-FDdialog.stack = FDdialog.Stack.new()   --!< \brief Dialogs stack                        \memberof Lua::FDdialog
+FDdialog.dialogs_dir = -1               --!< \brief Opaque handle to the directory containing the dialogs     \memberof Lua::FDdialog
+FDdialog.stack = FDdialog.Stack.new()   --!< \brief Dialogs stack                                             \memberof Lua::FDdialog
 --! \publicsection
 
 --! \fn void set_dialog_dir(string dir)
 --!
---! \brief Set dialogs' directory
+--! \brief Set dialogs' directory handle
 --!
---! \param dir Directory containing the dialogs
+--! \param dir_handle Opaque handle to the directory containing the dialogs
 --!
 --! \memberof Lua::FDdialog
 

po/po-src/de.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: freedroidrpg 0.15+git\n"
 "Report-Msgid-Bugs-To: http://www.freedroid.org\n"
-"POT-Creation-Date: 2014-04-02 22:30+0200\n"
+"POT-Creation-Date: 2014-04-02 23:37+0200\n"
 "PO-Revision-Date: 2013-09-01 00:37+0200\n"
 "Last-Translator: Freedroid <bugs@freedroid.org>\n"
 "Language-Team: German\n"
@@ -331,19 +331,19 @@ msgstr ""
 msgid "Out of [s]%s[v]!"
 msgstr ""
 
-#: src/init.c:872 src/saveloadgame.c:444
+#: src/init.c:897 src/saveloadgame.c:444
 msgid "--- Message Log ---"
 msgstr ""
 
-#: src/init.c:874
+#: src/init.c:899
 msgid "Starting tutorial."
 msgstr ""
 
-#: src/init.c:876
+#: src/init.c:901
 msgid "Starting new game."
 msgstr ""
 
-#: src/init.c:1174
+#: src/init.c:1199
 msgid ""
 "You are playing a Release Candidate.\n"
 "Strange bugs may still be present in the game.\n"
@@ -353,11 +353,11 @@ msgid ""
 "Good luck!\n"
 msgstr ""
 
-#: src/init.c:1192 src/init.c:1193
+#: src/init.c:1217 src/init.c:1218
 msgid "Game over.\n"
 msgstr ""
 
-#: src/init.c:1262 src/init.c:1263
+#: src/init.c:1287 src/init.c:1288
 msgid "Game won.\n"
 msgstr ""
 
@@ -1130,20 +1130,20 @@ msgstr ""
 msgid "Lost %d valuable circuits!"
 msgstr ""
 
-#: src/main.c:368
+#: src/main.c:369
 msgid "Overheating!"
 msgstr ""
 
 #. TRANSLATORS: Screen message, displayed below 'Automatically shutting down'
-#: src/main.c:373
+#: src/main.c:374
 msgid "due to near lethal overheating"
 msgstr ""
 
-#: src/main.c:374
+#: src/main.c:375
 msgid "Automatically shutting down"
 msgstr ""
 
-#: src/main.c:396
+#: src/main.c:397
 msgid "Weapon reloaded"
 msgstr ""
 
@@ -1474,15 +1474,15 @@ msgstr ""
 msgid "Delete existing Hero"
 msgstr ""
 
-#: src/misc.c:783
+#: src/misc.c:801
 msgid "GAME PAUSED"
 msgstr ""
 
-#: src/misc.c:784
+#: src/misc.c:802
 msgid "press p to resume"
 msgstr ""
 
-#: src/misc.c:1076
+#: src/misc.c:1094
 #, c-format
 msgid "Arrived at %s."
 msgstr ""

po/po-src/fr.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: freedroidrpg 0.15+git\n"
 "Report-Msgid-Bugs-To: http://www.freedroid.org\n"
-"POT-Creation-Date: 2014-04-02 22:30+0200\n"
+"POT-Creation-Date: 2014-04-02 23:37+0200\n"
 "PO-Revision-Date: 2013-09-03 00:10+0100\n"
 "Last-Translator: Freedroid <bugs@freedroid.org>\n"
 "Language-Team: French\n"
@@ -331,19 +331,19 @@ msgstr ""
 msgid "Out of [s]%s[v]!"
 msgstr ""
 
-#: src/init.c:872 src/saveloadgame.c:444
+#: src/init.c:897 src/saveloadgame.c:444
 msgid "--- Message Log ---"
 msgstr ""
 
-#: src/init.c:874
+#: src/init.c:899
 msgid "Starting tutorial."
 msgstr ""
 
-#: src/init.c:876
+#: src/init.c:901
 msgid "Starting new game."
 msgstr ""
 
-#: src/init.c:1174
+#: src/init.c:1199
 msgid ""
 "You are playing a Release Candidate.\n"
 "Strange bugs may still be present in the game.\n"
@@ -353,11 +353,11 @@ msgid ""
 "Good luck!\n"
 msgstr ""
 
-#: src/init.c:1192 src/init.c:1193
+#: src/init.c:1217 src/init.c:1218
 msgid "Game over.\n"
 msgstr ""
 
-#: src/init.c:1262 src/init.c:1263
+#: src/init.c:1287 src/init.c:1288
 msgid "Game won.\n"
 msgstr ""
 
@@ -1130,20 +1130,20 @@ msgstr ""
 msgid "Lost %d valuable circuits!"
 msgstr ""
 
-#: src/main.c:368
+#: src/main.c:369
 msgid "Overheating!"
 msgstr ""
 
 #. TRANSLATORS: Screen message, displayed below 'Automatically shutting down'
-#: src/main.c:373
+#: src/main.c:374
 msgid "due to near lethal overheating"
 msgstr ""
 
-#: src/main.c:374
+#: src/main.c:375
 msgid "Automatically shutting down"
 msgstr ""
 
-#: src/main.c:396
+#: src/main.c:397
 msgid "Weapon reloaded"
 msgstr ""
 
@@ -1474,15 +1474,15 @@ msgstr ""
 msgid "Delete existing Hero"
 msgstr ""
 
-#: src/misc.c:783
+#: src/misc.c:801
 msgid "GAME PAUSED"
 msgstr ""
 
-#: src/misc.c:784
+#: src/misc.c:802
 msgid "press p to resume"
 msgstr ""
 
-#: src/misc.c:1076
+#: src/misc.c:1094
 #, c-format
 msgid "Arrived at %s."
 msgstr ""

po/po-src/freedroidrpg.pot
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: freedroidrpg 0.15+git\n"
 "Report-Msgid-Bugs-To: http://www.freedroid.org\n"
-"POT-Creation-Date: 2014-04-02 22:30+0200\n"
+"POT-Creation-Date: 2014-04-02 23:37+0200\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -330,19 +330,19 @@ msgstr ""
 msgid "Out of [s]%s[v]!"
 msgstr ""
 
-#: src/init.c:872 src/saveloadgame.c:444
+#: src/init.c:897 src/saveloadgame.c:444
 msgid "--- Message Log ---"
 msgstr ""
 
-#: src/init.c:874
+#: src/init.c:899
 msgid "Starting tutorial."
 msgstr ""
 
-#: src/init.c:876
+#: src/init.c:901
 msgid "Starting new game."
 msgstr ""
 
-#: src/init.c:1174
+#: src/init.c:1199
 msgid ""
 "You are playing a Release Candidate.\n"
 "Strange bugs may still be present in the game.\n"
@@ -352,11 +352,11 @@ msgid ""
 "Good luck!\n"
 msgstr ""
 
-#: src/init.c:1192 src/init.c:1193
+#: src/init.c:1217 src/init.c:1218
 msgid "Game over.\n"
 msgstr ""
 
-#: src/init.c:1262 src/init.c:1263
+#: src/init.c:1287 src/init.c:1288
 msgid "Game won.\n"
 msgstr ""
 
@@ -1129,20 +1129,20 @@ msgstr ""
 msgid "Lost %d valuable circuits!"
 msgstr ""
 
-#: src/main.c:368
+#: src/main.c:369
 msgid "Overheating!"
 msgstr ""
 
 #. TRANSLATORS: Screen message, displayed below 'Automatically shutting down'
-#: src/main.c:373
+#: src/main.c:374
 msgid "due to near lethal overheating"
 msgstr ""
 
-#: src/main.c:374
+#: src/main.c:375
 msgid "Automatically shutting down"
 msgstr ""
 
-#: src/main.c:396
+#: src/main.c:397
 msgid "Weapon reloaded"
 msgstr ""
 
@@ -1473,15 +1473,15 @@ msgstr ""
 msgid "Delete existing Hero"
 msgstr ""
 
-#: src/misc.c:783
+#: src/misc.c:801
 msgid "GAME PAUSED"
 msgstr ""
 
-#: src/misc.c:784
+#: src/misc.c:802
 msgid "press p to resume"
 msgstr ""
 
-#: src/misc.c:1076
+#: src/misc.c:1094
 #, c-format
 msgid "Arrived at %s."
 msgstr ""

src/benchmark.c
@@ -85,8 +85,8 @@ static int loadship_bench()
 	int loop = 10;
 
 	// Find a ship file to load
-	char fp[2048];
-	find_file("levels.dat", MAP_DIR, fp, 0);
+	char fp[PATH_MAX];
+	find_file("levels.dat", MAP_DIR, fp);
 
 	// Load it many times
 	timer_start();
@@ -206,8 +206,8 @@ static int level_test()
 	int failed;
 
 	// Load default ship
-	char fp[2048];
-	find_file("levels.dat", MAP_DIR, fp, 0);
+	char fp[PATH_MAX];
+	find_file("levels.dat", MAP_DIR, fp);
 	LoadShip(fp, 0);
 
 	timer_start();

src/blocks.c
@@ -185,14 +185,14 @@ static void load_item_graphics(int item_type)
 {
 	SDL_Surface *original_img;
 	SDL_Surface *tmp_surf2 = NULL;
-	char fpath[2048];
-	char our_filename[2000];
+	char fpath[PATH_MAX];
+	char our_filename[PATH_MAX];
 	itemspec *spec = &ItemMap[item_type];
 
 	sprintf(our_filename, "items/%s", spec->item_inv_file_name);
 
 	// Load the inventory image	
-	find_file(our_filename, GRAPHICS_DIR, fpath, 0);
+	find_file(our_filename, GRAPHICS_DIR, fpath);
 
 	original_img = IMG_Load(fpath);
 	if (original_img == NULL) {
@@ -542,10 +542,10 @@ void Load_Enemy_Surfaces(void)
 	}
 
 
-	char fpath[2048];
+	char fpath[PATH_MAX];
 	char *Data;
 
-	find_file("enemy_surfaces.dat", MAP_DIR, fpath, 0);
+	find_file("enemy_surfaces.dat", MAP_DIR, fpath);
 	Data = ReadAndMallocAndTerminateFile(fpath, "*** End of this Freedroid data File ***");
 	get_enemy_surfaces_data(Data);
 	free(Data);

commit dc6efa7e422162f4d317b83ee4c3d398c400e422

Fix build error introduced by 21cd4a4297d8: LOCALEDIR is not defined if localization is disabled.

src/defs.h
@@ -1566,8 +1566,10 @@ enum data_dir_handle {
 	MAP_DIR,
 	TITLES_DIR,
 	DIALOG_DIR,
-	LUA_MOD_DIR,
+#ifdef ENABLE_NLS
 	LOCALE_DIR,
+#endif
+	LUA_MOD_DIR,
 	LAST_DATA_DIR
 };
 

src/misc.c
@@ -67,8 +67,10 @@ struct data_dir data_dirs[] = {
 	[MAP_DIR]=     { "map",         "" },
 	[TITLES_DIR]=  { "map/titles",  "" },
 	[DIALOG_DIR]=  { "dialogs",     "" },
-	[LUA_MOD_DIR]= { "lua_modules", "" },
-	[LOCALE_DIR]=  { "locale",      "" }
+#ifdef ENABLE_NLS
+	[LOCALE_DIR]=  { "locale",      "" },
+#endif
+	[LUA_MOD_DIR]= { "lua_modules", "" }
 };
 #define WELL_KNOWN_DATA_FILE "lua_modules/FDdialog.lua"
 
@@ -630,7 +632,9 @@ int init_data_dirs_path()
 
 	// Directories to look for the data dirs
 	char *top_data_dir[]   = { ".", "..", "../..", FD_DATADIR };
+#ifdef ENABLE_NLS
 	char *top_locale_dir[] = { ".", "..", "../..", LOCALEDIR };
+#endif
 	int slen = sizeof(top_data_dir)/sizeof(top_data_dir[0]);
 
 	// To find the root of the data dirs, we search a well known file that is
@@ -641,7 +645,11 @@ int init_data_dirs_path()
 		if ((f = fopen(file_path, "r")) != NULL) {
 			// File found, so now fill the data dir paths
 			for (j = 0; j < LAST_DATA_DIR; j++) {
-				char *dir = (j == LOCALE_DIR) ? top_locale_dir[i] : top_data_dir[i];
+				char *dir = top_data_dir[i];
+#ifdef ENABLE_NLS
+				if (j == LOCALE_DIR)
+					dir = top_locale_dir[i];
+#endif
 				int nb = snprintf(data_dirs[j].path, PATH_MAX, "%s/%s", dir, data_dirs[j].name);
 				if (nb >= PATH_MAX) {
 					ErrorMessage(__FUNCTION__, "data_dirs[].path is not big enough to store the following path: %s/%s",