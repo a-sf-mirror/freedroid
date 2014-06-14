83a83d40ed8c3ed87330acfa4d2bd6339c9c7efb
fluzz <fluzz@gmx.com>
2014-06-14 11:22:23 +0200

    Localization updates

---
+++ map/titles/de/Credits.lua
@@ -0,0 +1,120 @@
+---------------------------------------------------------------------
+-- This file is part of Freedroid
+--
+-- Freedroid is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- Freedroid is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Freedroid; see the file COPYING. If not, write to the
+-- Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+-- MA 02111-1307 USA
+----------------------------------------------------------------------
+
+title_screen{
+background = "credits.jpg",
+song = "HellFortressTwo.ogg",
+text = [[
+            FreedroidRPG 0.15
+
+PROGRAMMIERUNG:
+
+        Maria Grazia Alastra
+        Pedro Arana
+        Catalin Badea
+        Bryan C
+        Samuel Degrande
+        Arthur Huillet
+        Stefan Kangas
+        Jozef Kucia
+        Pavaluca Matei
+        Michael Mendelson
+        Esa-Matti Mourujarvi
+        Nicolas Pepin-Perreault
+        Samuel Pitoiset
+        Alexander Solovets
+        Philippe Tetar
+
+SPIELINHALT:
+
+        Matthias Krüger
+        Miles McCammon
+        Sebastian Offermann
+        Kurtis Parramore
+        Niklas Spille
+        JK Wood
+        James
+        Starminn
+        Thor
+        rudi_s
+        Infrared
+
+GRAFIKEN:
+
+        Infrared
+
+RELEASE MANAGEMENT:
+
+        Arthur Huillet
+        Miles McCammon
+
+
+
+            SEIT ÄONEN MIT DABEI:
+
+PROGRAMMIERUNG:
+
+        Johannes Prix
+        Reinhard Prix
+        Quentin Pradet
+        jcoral
+        Pierre Bourdon
+        Simon Castellan
+        Ari Mustonen
+
+SPIELINHALT:
+
+        Stefan Huszics
+
+GRAFIKEN:
+
+        Bastian Salmela
+
+DIALOGE:
+
+        Karol Swietlicki
+
+MUSIK:
+
+        Nick "Nario" Hagman
+        Arvid Picciani
+        "The Beginning"
+            by 4t thieves
+        "Daybreak"
+            by realsmokers
+        "Bleostrada"
+            by stud
+
+VERSCHIEDENES:
+
+        Mbrola - text-to-speech
+        eSpeak - text-to-speech
+        Ryan 'simcop2387' Voots
+        Andrew A. Gill
+        Zombie Ryushu
+        Ted Cipicchio
+        The Doctor
+        Simon Newton
+        Clint Herron
+        Hike Danakian
+        Ian Griffiths
+        David Kremer
+        Armin Winterer
+]]
+}
