a-sf-mirror/freedroid

commit d3d7e6bb8a96ccb2703ba22b72573bc10b4dd6b5

Our bitmap fonts are subsets of the ISO-8859-15 8-bit codeset. In order to be able to use non Latin-9 alphabets, this patch adds handling of charset encodings.
The encoding to use for a given language is defined in
map/languages.lua. If, for a given language, an encoding is not defined, it
defaults to ASCII.

A new structure of the graphics/font subdir is used:
- fonts in graphics/font are 7-bit ASCII fonts.
- fonts in 8-bit encodings are placed into graphics/font/<codeset name>
- if a font file is not found in a <codeset name> subdir, the ASCII version is
  used

(This patch does not include any change to graphics/font, but this is not an
 issue due to that 3rd point)

For instance, our current fonts will be moved into graphics/font/ISO-8859-15

When a user changes the current language via the 'Languages' menu, the
current encoding used to read and convert gmo files is changed accordingly.

Note that currently the fonts are not reloaded, the game has to be
restarted. A future patch will enable in-game reloading of fonts.

src/lang.c
@@ -31,10 +31,64 @@
 #include "global.h"
 #include "proto.h"
 
+static struct codeset _default_codeset = { "_default_", "ASCII" };
+static char *_current_encoding = NULL;
+
+/**
+ * Extract the subforms of a locale, by removing the trailing codes one at a time.
+ *
+ * Called in a loop, this function generates all forms of a locale, from the
+ * most specialized to the language only. 'next_tokens' is used internally
+ * to know which part to extract on the next call. On the first call, it must
+ * contain a NULL pointer. So a typical usage is:
+ * \code
+ * char *next_tokens = NULL;
+ * char *extracted_locale;
+ * while ((extracted_locale = lang_extract_next_part("fr_FR.ISO-8859-1@euro", &next_tokens)) != NULL) {
+ *   ... do something ...
+ * }
+ * \endcode
+ * This code returns in turn: "fr_FR.ISO-8859-1@euro", "fr_FR.ISO-8859-1", "fr_FR", "fr", NULL.
+ *
+ * \param locale Pointer to the locale to parse
+ * \param next_token Pointer to a char pointer, used internally. Must points to NULL on the first use.
+ *
+ * \return A pointer to the current subform of the locale. On first call, returns the whole locale.
+ *         When no more parts have to be extracted, returns NULL.
+ */
+char *lang_extract_next_subform(const char *locale, char **next_tokens)
+{
+	static char *tokens = "@._";
+	static char *extract = NULL;
+
+	if (*next_tokens == NULL) {
+		*next_tokens = tokens;
+		if (extract)
+			free(extract);
+		extract = my_strdup(locale);
+		return extract;
+	} else if (**next_tokens == '\0') {
+		free(extract);
+		return NULL;
+	}
+
+	while (**next_tokens) {
+		char *ptr = strchr(extract, **next_tokens);
+		if (ptr) {
+			*ptr = '\0';
+			return extract;
+		}
+		(*next_tokens)++;
+	}
+	free(extract);
+	return NULL;
+}
+
 /**
  * Set the locale used by the game for text translation.
  *
- * On success, the locale is set into the game config.
+ * On success, the locale is set into the game config. The charset encoding
+ * to use is computed, and stored internally.
  * Note: GameConfig.locale is set to "" to denote the use of the system
  * default locale.
  *
@@ -44,41 +98,95 @@
 void lang_set(const char *locale)
 {
 #ifdef ENABLE_NLS
-	// When called with an actual locale name, setlocale() checks if the
-	// locale is configured on the system.
-	// Unfortunately, some distros such as Debian and its children, or Arch,
-	// only have a few locales pre-configured.
-	//
-	// So in order to be able to use all our translations, we rather rely
+	// Usually setlocale() is called to define the locale to use for
+	// localization. However, when called with a locale name, setlocale()
+	// checks if that locale is configured on the system, and if not it does
+	// not change the current locale.
+	// Unfortunately, some distros such as Debian and its derivatives, or Arch,
+	// only have a few pre-configured locales.
+	// So in order to be able to use all our translations, even if the
+	// corresponding locales are not installed on the system, we rather rely
 	// on the LANGUAGE envvar and ask gettext to use the system default
-	// locale. (gettext() gives precedence to LANGUAGE over any other envvar,
-	// see GNU gettext manual)
+	// locale. gettext() gives precedence to LANGUAGE over any other envvar
+	// (see GNU gettext manual), avoiding to have to install any locale.
 
+	char *applied_locale = NULL;
+
+	// If a specific locale was requested, we set the LANGUAGE envvar.
+	if (locale && strlen(locale)) {
+		if (fd_setenv("LANGUAGE", locale, TRUE) != 0)
+			return;
+		applied_locale = (char *)locale;
+	}
+
+	// If we were asked to use the system default locale, LANGUAGE is unset, and
+	// we configure gettext() to use the LC_MESSAGES envvar.
 	if (!locale || strlen(locale) == 0) {
 		if (fd_unsetenv("LANGUAGE") != 0)
 			return;
-	} else {
-		if (fd_setenv("LANGUAGE", locale, TRUE) != 0)
-			return;
+		applied_locale = setlocale(LC_MESSAGES, "");
+		if (!applied_locale) {
+			error_message(__FUNCTION__,
+					"You asked to use the system default local, defined by the LC_MESSAGES envvar.\n"
+					"But that locale seems to be unknown on your system. Switching to C locale.",
+					PLEASE_INFORM);
+			setlocale(LC_MESSAGES, "C");
+			applied_locale = "";
+		}
 	}
 
-	// Use LANGUAGE or system default locale
-	if (!setlocale(LC_MESSAGES, "")) {
-		error_message(__FUNCTION__, "Error when calling setlocale() to set %s locale", PLEASE_INFORM, (locale) ? locale : "NULL");
-		return;
-	}
-
+	// Save the requested locale in GameConfig
 	// 'locale' and GameConfig.locale can possibly be the same pointer.
-	// So we make a copy of 'locale' to avoid any issue.
-	char *ll = (locale) ? strdup(locale) : NULL;
-	if (GameConfig.locale)
-		free(GameConfig.locale);
-	if (ll)
-		GameConfig.locale = strdup(ll);
-	else
-		GameConfig.locale = strdup("");
-	if (ll)
-		free(ll);
+	// In that case, do nothing.
+	if (locale != GameConfig.locale) {
+		if (GameConfig.locale)
+			free(GameConfig.locale);
+		if (locale && strlen(locale))
+			GameConfig.locale = my_strdup(locale);
+		else
+			GameConfig.locale = my_strdup("");
+	}
+
+	// Search for the charset encoding to use, unless we default to "C" locale
+	// in which case we will use the default encoding.
+	char *new_encoding = NULL;
+	if (applied_locale && strlen(applied_locale)) {
+		char *tok = NULL;
+		while (!new_encoding) {
+			// Remove trailing parts of the locale, to get all the subforms of
+			// the locale, one at a time
+			char *subform = lang_extract_next_subform(applied_locale, &tok);
+			if (!subform) break;
+
+			// Check if the extracted sub-definition of the locale is one of the available
+			// codesets
+			int i;
+			for (i = 0; i < lang_codesets.size; i++) {
+				struct codeset *cs = dynarray_member(&lang_codesets, i, sizeof(struct codeset));
+				if (!strcmp(subform, cs->language)) {
+					new_encoding = cs->encoding;
+					break;
+				}
+			}
+		}
+		if (!new_encoding) {
+			error_message(__FUNCTION__, "The charset encoding of your locale (%s) is not available. Defaulting to %s.",
+					NO_REPORT, applied_locale, _default_codeset.encoding);
+			new_encoding = _default_codeset.encoding;
+		}
+	} else {
+		new_encoding = _default_codeset.encoding;
+	}
+
+	// If a new codeset is to be used, gettext conversion has to be changed and
+	// font bitmaps have to be reloaded
+	if (strcmp(new_encoding, _current_encoding)) {
+		_current_encoding = new_encoding;
+		bind_textdomain_codeset("freedroidrpg", _current_encoding);
+		bind_textdomain_codeset("freedroidrpg-dialogs", _current_encoding);
+		bind_textdomain_codeset("freedroidrpg-data", _current_encoding);
+		// TODO: reload font bitmaps - Currently, the game needs to be restarted
+	}
 #endif
 }
 
@@ -103,6 +211,20 @@ char *lang_get()
 }
 
 /**
+ * Return the charset encoding to be used to load font bitmaps.
+ *
+ * \return Pointer to the encoding.
+ */
+char *lang_get_encoding()
+{
+#ifdef ENABLE_NLS
+	return _current_encoding;
+#else
+	return _default_codeset.encoding;
+#endif
+}
+
+/**
  * Initialize localization.
  *
  * The game is started in the system default locale.
@@ -117,6 +239,8 @@ void lang_init()
 	setlocale(LC_ALL, "C");
 	setlocale(LC_MESSAGES, "");
 
+	_current_encoding = _default_codeset.encoding;
+
 	const char *localedir = data_dirs[LOCALE_DIR].path;
 	if (strlen(localedir) == 0) {
 		error_message(__FUNCTION__, "Locale dir not found. Disabling localization.",
@@ -124,15 +248,15 @@ void lang_init()
 		setlocale(LC_MESSAGES, "C");
 	} else {
 		// i18n text domain declarations.
-		// Note: our bitmap fonts are not utf-8 compliant, but contain part of the
-		// latin-1 alphabet. So we enforce a conversion to iso-8859-1.
+		// Note: our bitmap fonts are not utf-8 compliant, but contain a subset
+		// of the iso-8859 charset encodings. So we enforce a conversion.
 
 		bindtextdomain("freedroidrpg", localedir);
-		bind_textdomain_codeset("freedroidrpg", "ISO-8859-1");
+		bind_textdomain_codeset("freedroidrpg", _current_encoding);
 		bindtextdomain("freedroidrpg-dialogs", localedir);
-		bind_textdomain_codeset("freedroidrpg-dialogs", "ISO-8859-1");
+		bind_textdomain_codeset("freedroidrpg-dialogs", _current_encoding);
 		bindtextdomain("freedroidrpg-data", localedir);
-		bind_textdomain_codeset("freedroidrpg-data", "ISO-8859-1");
+		bind_textdomain_codeset("freedroidrpg-data", _current_encoding);
 
 		// Default domain to use, if none is specified
 		textdomain("freedroidrpg");

map/languages.lua
@@ -22,9 +22,27 @@
 ]]--
 
 languages{
-	{ name = "Brasileiro", locale = "pt_BR" },
-	{ name = "Deutsch",  locale = "de_DE" },
-	{ name = "English (US)",  locale = "en_US" },
-	{ name = "Francais", locale = "fr_FR" },
-	{ name = "Svenska", locale = "sv_SV" },
+	-- name: Language name, as displayed in the Languages Menu
+	-- locale: Locale name, of the form language_territory
+	--         (language is an ISO 639 language code, territory is an
+	--         ISO 3166 country code)
+	{ name = "Brasileiro",   locale = "pt_BR" },
+	{ name = "Deutsch",      locale = "de_DE" },
+	{ name = "English (US)", locale = "en_US" },
+	{ name = "Francais",     locale = "fr_FR" },
+	{ name = "Svenska",      locale = "sv_SV" },
+}
+
+codesets{
+	-- language: can be a simple 'language code', to define a default
+	--           encoding for all the locales of that language group,
+	--           or can be 'language_territory' to define a specific encoding.
+	-- encoding: one of the font bitmap encoding provided by the game (see
+	--           graphics/font).
+	-- Note: default encoding is ASCII
+	{ language = "de", encoding = "ISO-8859-15" },
+	{ language = "en", encoding = "ISO-8859-15" },
+	{ language = "fr", encoding = "ISO-8859-15" },
+	{ language = "pt", encoding = "ISO-8859-15" },
+	{ language = "sv", encoding = "ISO-8859-15" },
 }

src/BFont.c
@@ -13,8 +13,6 @@
 #include "proto.h"
 #include "global.h"
 
-#define FIRST_FONT_CHAR '!'
-
 /**
  * Initialize a font.
  */

src/BFont.h
@@ -10,6 +10,7 @@
 #include "system.h"
 #include "struct.h"
 
+#define FIRST_FONT_CHAR '!'
 #define MAX_CHARS_IN_FONT 256
 
 typedef struct BFont_Info {

src/defs.h
@@ -1568,6 +1568,7 @@ enum chat_context_state {
 // Indexes of known data dirs (used in conjunction with data_dirs[])
 enum data_dir_handle {
 	GRAPHICS_DIR = 0,
+	FONT_DIR,
 	SOUND_DIR,
 	MUSIC_DIR,
 	MAP_DIR,

src/font.c
@@ -69,14 +69,17 @@ void init_fonts(void)
 	char fpath[PATH_MAX];
 
 	for (i = 0; i < sizeof(fonts_def)/sizeof(fonts_def[0]); i++) {
-		char constructed_fname[PATH_MAX];
-		sprintf(constructed_fname, "font/%s", fonts_def[i].filename);
-		find_file(constructed_fname, GRAPHICS_DIR, fpath, PLEASE_INFORM | IS_FATAL);
+		if (!find_encoded_file(fonts_def[i].filename, FONT_DIR, fpath, PLEASE_INFORM | IS_FATAL)) {
+			error_message(__FUNCTION__, "A Bfont file was not found (filename: %s - encoding: %s).",
+					PLEASE_INFORM, fonts_def[i].filename, lang_get_encoding());
+			continue;
+		}
 
 		int rtn = load_bfont(fpath, &fonts_def[i].font);
 		if (rtn == FALSE) {
-			error_message(__FUNCTION__, "A font file for the BFont library could not be loaded (%s).",
-					PLEASE_INFORM | IS_FATAL, fonts_def[i].filename);
+			error_message(__FUNCTION__, "A Bfont file could not be loaded (filename: %s - encoding: %s).",
+					PLEASE_INFORM | IS_FATAL, fonts_def[i].filename, lang_get_encoding());
+			return;
 		}
 
 		*fonts_def[i].font_ref = &fonts_def[i].font;

src/global.h
@@ -392,6 +392,14 @@ EXTERN struct dynarray bullet_specs;
 #endif
 
 EXTERN struct dynarray lang_specs;
+EXTERN struct dynarray lang_codesets;
+
+#undef EXTERN
+#ifdef _init_c
+#define EXTERN
+#else
+#define EXTERN extern
+#endif
 
 EXTERN struct dynarray difficulties;
 

src/init.c
@@ -401,6 +401,7 @@ static void load_fdrpg_config()
 
 	// Load the languages specs
 	dynarray_free(&lang_specs);
+	dynarray_free(&lang_codesets);
 	if (find_file("languages.lua", MAP_DIR, fpath, PLEASE_INFORM)) {
 		run_lua_file(LUA_CONFIG, fpath);
 	}

commit ca92e72b4977ac064328259219707bddd2950e6a

Translations are no more immediately changed when a new language is selected, due to a bug introduced in commit d3d7e6bb8a: setlocale() is not called when a new language is selected. Fixed by this commit.

src/lang.c
@@ -97,6 +97,7 @@ char *lang_extract_next_subform(const char *locale, char **next_tokens)
 
 void lang_set(const char *locale)
 {
+	DebugPrintf(-1, "switch to <%s>\n", (locale) ? (locale) : "NULL");
 #ifdef ENABLE_NLS
 	// Usually setlocale() is called to define the locale to use for
 	// localization. However, when called with a locale name, setlocale()
@@ -116,6 +117,7 @@ void lang_set(const char *locale)
 	if (locale && strlen(locale)) {
 		if (fd_setenv("LANGUAGE", locale, TRUE) != 0)
 			return;
+		setlocale(LC_MESSAGES, ""); // gettext() will use LANGUAGE
 		applied_locale = (char *)locale;
 	}
 
@@ -124,7 +126,7 @@ void lang_set(const char *locale)
 	if (!locale || strlen(locale) == 0) {
 		if (fd_unsetenv("LANGUAGE") != 0)
 			return;
-		applied_locale = setlocale(LC_MESSAGES, "");
+		applied_locale = setlocale(LC_MESSAGES, ""); // gettext() will use envvars
 		if (!applied_locale) {
 			error_message(__FUNCTION__,
 					"You asked to use the system default local, defined by the LC_MESSAGES envvar.\n"
@@ -150,7 +152,7 @@ void lang_set(const char *locale)
 	// Search for the charset encoding to use, unless we default to "C" locale
 	// in which case we will use the default encoding.
 	char *new_encoding = NULL;
-	if (applied_locale && strlen(applied_locale)) {
+	if (applied_locale && strlen(applied_locale) != 0) {
 		char *tok = NULL;
 		while (!new_encoding) {
 			// Remove trailing parts of the locale, to get all the subforms of