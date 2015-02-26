a-sf-mirror/freedroid

commit ad50da9caa81ebb5c4ccc70cfb754f14e5db5a00

Convert encoding of Title texts from UTF-8 to the encoding of the currently selected language, before to display them.

src/init.c
@@ -46,6 +46,10 @@
 #include <shlobj.h>
 #endif
 
+#ifdef ENABLE_NLS
+#include <iconv.h>
+#endif
+
 void Init_Game_Data(void);
 void UpdateCountersForThisFrame();
 void DoAllMovementAndAnimations(void);
@@ -178,6 +182,50 @@ void PlayATitleFile(char *Filename)
 	if (find_localized_file(Filename, TITLES_DIR, fpath, PLEASE_INFORM)) {
 		set_lua_ctor_upvalue(LUA_CONFIG, "title_screen", &screen);
 		run_lua_file(LUA_CONFIG, fpath);
+
+#ifdef ENABLE_NLS
+		// Convert the title_screen(s text to selected charset encoding
+		iconv_t converter = iconv_open(lang_get_encoding(), "UTF-8");
+		if (converter == (iconv_t)-1) {
+			error_once_message(ONCE_PER_GAME, __FUNCTION__,
+			                   "Error on iconv_open() (encoding: %s): %s",
+			                   NO_REPORT, lang_get_encoding(), strerror(errno));
+		} else {
+			char *in_ptr = screen.text;
+			size_t in_len = strlen(screen.text);
+			// We currently only have 8 bits encodings, so we should not need
+			// an output buffer larger than the input one.
+			char *converted_text = MyMalloc(in_len);
+			char *out_ptr = converted_text;
+			size_t out_len = in_len;
+
+			size_t nb = iconv(converter, &in_ptr, &in_len, &out_ptr, &out_len);
+
+			// In case of error, use the un-converted text
+			if (nb == (size_t)-1) {
+				locale_t c_locale = newlocale(LC_MESSAGES, "C", (locale_t)0);
+				if (errno == EILSEQ || errno == EINVAL) {
+					char invalid_text[23];
+					strncpy(invalid_text, in_ptr, 20);
+					strcat(invalid_text, "...");
+					error_once_message(ONCE_PER_GAME, __FUNCTION__,
+				                       "Error during Title text conversion (title: %s - encoding: %s): %s\n"
+					                   "Invalid sequence:\n--->%s<---",
+									   PLEASE_INFORM, Filename, lang_get_encoding(), strerror_l(errno, c_locale), invalid_text);
+				} else {
+					error_once_message(ONCE_PER_GAME, __FUNCTION__,
+				                       "Error during Title text conversion (title: %s - encoding: %s): %s",
+									   NO_REPORT, Filename, lang_get_encoding(), strerror_l(errno, c_locale));
+				}
+				freelocale(c_locale);
+				free(converted_text);
+			} else {
+				// Replace the title's text by the converted one
+				free(screen.text);
+				screen.text = converted_text;
+			}
+		}
+#endif
 
 		// Remove trailing whitespaces and carriage returns.
 		char *ptr = screen.text + strlen(screen.text) - 1;

commit 80876a6fa132cf8ee8ecb8ffcc9f424673dd6fe9

Fix the build and execution error which was introduced by ad50da9caa81 and reverted by b4e97350f1.

configure.ac
@@ -19,6 +19,7 @@ XIPH_C_COMPILER_IS_CLANG
 AM_PATH_PYTHON([2.6])
 AM_GNU_GETTEXT_VERSION([0.18])
 AM_GNU_GETTEXT([external])
+AM_ICONV
 
 AC_HEADER_STDC
 

src/Makefile.am
@@ -16,7 +16,7 @@ freedroidRPG_LDADD += $(top_builddir)/win32/freedroidRPG.coff
 endif
 
 if USE_NLS
-freedroidRPG_LDADD += @LIBINTL@
+freedroidRPG_LDADD += @LIBINTL@ @LIBICONV@
 AM_CPPFLAGS += -DLOCALEDIR=\"$(localedir)\"
 endif
 

src/init.c
@@ -183,7 +183,6 @@ void PlayATitleFile(char *Filename)
 		set_lua_ctor_upvalue(LUA_CONFIG, "title_screen", &screen);
 		run_lua_file(LUA_CONFIG, fpath);
 
-#if 0 // Fluzz: Need some fixing (MacOS needs -liconv, asan crash on strlen()...)
 #ifdef ENABLE_NLS
 		// Convert the title_screen(s text to selected charset encoding
 		iconv_t converter = iconv_open(lang_get_encoding(), "UTF-8");
@@ -196,7 +195,7 @@ void PlayATitleFile(char *Filename)
 			size_t in_len = strlen(screen.text);
 			// We currently only have 8 bits encodings, so we should not need
 			// an output buffer larger than the input one.
-			char *converted_text = MyMalloc(in_len);
+			char *converted_text = MyMalloc(in_len+1);
 			char *out_ptr = converted_text;
 			size_t out_len = in_len;
 
@@ -204,7 +203,6 @@ void PlayATitleFile(char *Filename)
 
 			// In case of error, use the un-converted text
 			if (nb == (size_t)-1) {
-				locale_t c_locale = newlocale(LC_MESSAGES, "C", (locale_t)0);
 				if (errno == EILSEQ || errno == EINVAL) {
 					char invalid_text[23];
 					strncpy(invalid_text, in_ptr, 20);
@@ -212,13 +210,12 @@ void PlayATitleFile(char *Filename)
 					error_once_message(ONCE_PER_GAME, __FUNCTION__,
 				                       "Error during Title text conversion (title: %s - encoding: %s): %s\n"
 					                   "Invalid sequence:\n--->%s<---",
-									   PLEASE_INFORM, Filename, lang_get_encoding(), strerror_l(errno, c_locale), invalid_text);
+									   PLEASE_INFORM, Filename, lang_get_encoding(), strerror(errno), invalid_text);
 				} else {
 					error_once_message(ONCE_PER_GAME, __FUNCTION__,
 				                       "Error during Title text conversion (title: %s - encoding: %s): %s",
-									   NO_REPORT, Filename, lang_get_encoding(), strerror_l(errno, c_locale));
+									   NO_REPORT, Filename, lang_get_encoding(), strerror(errno));
 				}
-				freelocale(c_locale);
 				free(converted_text);
 			} else {
 				// Replace the title's text by the converted one
@@ -226,7 +223,7 @@ void PlayATitleFile(char *Filename)
 				screen.text = converted_text;
 			}
 		}
-#endif
+		iconv_close(converter);
 #endif
 
 		// Remove trailing whitespaces and carriage returns.