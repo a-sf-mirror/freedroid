a-sf-mirror/freedroid

commit 2afcf2afca58f7713abf75efd5b78a9f3dcc8188

Win32 does not understand the "hh" format string's length specifier.

src/savestruct_internal.c
@@ -77,7 +77,11 @@ define_read_xxx(uint8_t);
   * \param strout The auto_string to be filled
   * \param data   Pointer to the data to write
   */
-define_write_xxx(uint8_t, "%hhu");
+#if __WIN32__
+define_write_xxx(uint8_t, "%hu");
+#else
+define_write_xxx(uint8_t, "%hu");
+#endif
 
 /**
  * \fn void read_uint16_t(lua_State *L, int index, uint16_t *data)

commit ac3b271fdfca2584a595d0a6d2a4369191a5c667

Fix wrong copy-paste introduced with commit 2afcf2afca58.

src/savestruct_internal.c
@@ -80,7 +80,7 @@ define_read_xxx(uint8_t);
 #if __WIN32__
 define_write_xxx(uint8_t, "%hu");
 #else
-define_write_xxx(uint8_t, "%hu");
+define_write_xxx(uint8_t, "%hhu");
 #endif
 
 /**