5b8f789e36fe72e33b5a1c6a0e461758e073e1d4
Scott Furry <scott.wl.furry@gmail.com>
2017-03-16 14:23:41 -0300

    Add mission_description field on quests.lua
    Previous quests.dat file was parsed manually (wiki parsing) for descriptions of quests.
    New luaized quest file leaves those descriptions as lua comments and are
    unavailable to PGPG unless the file is parsed manually.
    Diff moves descriptions to an easily accessible variable.
    Also, minor re-format of one element was performed.
    Aditionally, fix a wrong completion_ccode reported by fluzz.

--- data/storyline/act1/quests.lua
+++ data/storyline/act1/quests.lua
@@ -23,15 +23,13 @@
 -- something cool, please send your file in to the Freedroid project.
 
 mission_list {
-
--- Find a cure for Bender, suffering from brain enlargement pills
 {
 	mission_name = _"Bender's problem",
+	mission_description = "Find a cure for Bender, suffering from brain enlargement pills",
 },
-
--- Retrieve the toolkit lost by Dixon
 {
 	mission_name = _"The yellow toolkit",
+	mission_description = "Retrieve Dixon's lost toolkit",
 	completion_code = [=[
 		change_obstacle_state("Town-Solar-1", "enabled")
 		change_obstacle_state("Town-Solar-2", "enabled")
@@ -43,21 +41,17 @@ mission_list {
 		change_obstacle_state("Town-Solar-8", "enabled")
 	]=],
 },
-
--- Find some energy crystals for the cook Michelangelo
-
 {
 	mission_name = _"Anything but the army snacks, please!",
+	mission_description = "Find energy crystals for Michelangelo",
 },
-
--- Defeat the bots in the novice arena
 {
 	mission_name = _"Novice Arena",
+	mission_description = "Defeat the bots in the Novice Arena",
 },
-
--- Defeat the bots in the master arena
 {
 	mission_name = _"Time to say goodnight",
+	mission_description = "Defeat the bots in the Master Arena",
 	kill_droids_marked = 1030,
 	completion_code = [=[
 		change_obstacle_state("MasterArenaExitDoor", "opened")
@@ -66,19 +60,17 @@ mission_list {
 		update_quest("Time to say goodnight", _"I managed to win the fight in the master arena. Am I really that good, or did I cheat?")
 	]=],
 },
-
--- Clean out the first level of the warehouse
 {
 	mission_name = _"Opening a can of bots...",
+	mission_description = "Clean out the first sub-level of the warehouse",
 	must_clear_level = 1,
 	completion_code = [=[
 		update_quest("Opening a can of bots...", _"The first level of the warehouse has been cleared of bots; Dixon's men can now safely retrieve the supplies from here.")
 	]=],
 },
-
--- Clean out the second level under Kevins station
 {
 	mission_name = _"And there was light...",
+	mission_description = "Clean out the second sub-level of the Kevin's Complex",
 	must_clear_level = 18,
 	completion_code = [=[
 		update_quest("And there was light...", _"Hope everything is back to normal now. I better go check with Kevin.")
@@ -86,32 +78,25 @@ mission_list {
 		display_big_message(_"Level cleared!")
 	]=],
 },
-
--- Take Kevins data cube to the cluster maintenance people inside the red guard
--- complex
 {
 	mission_name = _"A kingdom for a cluster!",
+	mission_description = "Take Kevin's data cube to the cluster maintenance people inside the Red Guard complex",
 },
-
--- Reach the disruptor shield generator and take control of it
 {
 	mission_name = _"Opening access to MS Office",
+	mission_description = "Reach the disruptor shield generator and take control of it",
 },
-
--- ENDGAME MISSION - propagate faulty firmware update.
--- This mission can only be finished through a talk with the firmware update server.
 {
 	mission_name = _"Propagating a faulty firmware update",
+	mission_description = "Upload faulty firmware to the firmware update server.",
 },
-
--- Bring SADD dilithium crystals 
 {
 	mission_name = _"SADD's power supply",
+	mission_description = "Bring energy crystals to SADD",
 },
-
--- Free Tania & Escort Tania to the town
 {
 	mission_name = _"Tania's Escape",
+	mission_description = "Free Tania from underground facility and escort her to the town",
 	kill_droids_marked = 2035,
 	completion_code = [=[
 		if (npc_dead("Tania")) then
@@ -132,135 +117,116 @@ mission_list {
 		end
 	]=],
 },
-
--- Help Stone pay tax
 {
 	mission_name = _"Saving the shop",
+	mission_description = "Help Stone pay her tax bill",
 	completion_code = [=[
 		sell_item("Shotgun shells", 1, "Stone")
 		sell_item(".22 LR Ammunition", 1, "Stone")
 	]=],
 },
-
--- Find Koan
 {
 	mission_name = _"Doing Duncan a favor",
+	mission_description = "Find Koan",
 },
-
--- Learn to Move
 {
 	mission_name = _"Tutorial Movement",
+	mission_description = "Learn to Move",
 },
-
--- Defeat the shooting range bots in the Tutorial
 {
 	mission_name = _"Tutorial Melee",
+	mission_description = "Learn to fight bots hand-to-hand",
 	kill_droids_marked = 2037,
 	completion_code = [=[
 		display_big_message(_"Melee droids destroyed!")
 	]=],
 },
-
--- Defeat the shooting range bots in the Tutorial
 {
 	mission_name = _"Tutorial Shooting",
+	mission_description = "Learn to shoot bots",
 	kill_droids_marked = 4037,
 	completion_code = [=[
 		display_big_message(_"Shooting range cleared!")
 	]=],
 },
-
--- Defeat the hacking area bots in the Tutorial
 {
 	mission_name = _"Tutorial Hacking",
+	mission_description = "Learn to hack bots",
 	kill_droids_marked = 3037,
 	completion_code = [=[
 		display_big_message(_"Hacking area cleared!")
 	]=],
 },
-
--- Find Kevin
 {
 	mission_name = _"A strange guy stealing from town",
+	mission_description = "Find Kevin",
 },
-
--- Deliver list from Francis to Spencer
 {
 	mission_name = _"Deliverance",
+	mission_description = "Deliver list from Francis to Spencer",
 },
-
--- Deliver a nice meal to Will Gapes
 {
 	mission_name = _"Gapes Gluttony",
+	mission_description = "Deliver a nice meal to Will Gapes",
 },
-
--- Upgrading Items in the Tutorial
 {
 	mission_name = _"Tutorial Upgrading Items",
+	mission_description = "Learn to upgrade items",
 	completion_code = [=[
 		display_console_message(_"Tutorial Upgrading Items finished!")
 	]=],
 },
-
--- Clean out the old server room
 {
 	mission_name = _"Droids are my friends",
+	mission_description = "Clean out the old server room",
 	must_clear_level = 58,
 	completion_code = [=[
 		update_quest("Droids are my friends", _"The old server room is now secured.")
 	]=],
 },
-
--- Save the town from nuclear disaster
 {
 	mission_name = _"An Explosive Situation",
+	mission_description = "Save the town from nuclear disaster",
 	completion_code = [=[
 		update_quest("An Explosive Situation", _"Not only is the town safe, but apparently so is Ewald's 296 droid. Oh joy.")
 	]=],
 },
-
--- Hack the Hell Fortress Gate Access Server and find the gate.
 {
 	mission_name = _"Open Sesame",
+	mission_description = "Hack the Hell Fortress Gate Access Server and find the gate.",
 },
-
--- Get a red Toolbox back to Jenniffer
 {
 	mission_name = _"Jennifer's Toolbox",
+	mission_description = "Find red toolbox and return it to Jennifer",
 },
-
--- This is a test quest
 {
 	mission_name = _"24_label_test_quest",
+	mission_description = "This is a test quest",
 },
-
--- This is a test quest
 {
 	mission_name = _"24_dude_test_quest",
+	mission_description = "This is also a test quest",
 },
-
--- Another test quest
 {
 	mission_name = _"24_guy_death_quest",
+	mission_description = "This is another test quest",
 	kill_droids_marked = 2025,
 	completion_code = [=[
 		display_big_message("24 guy death quest solved")
 	]=],
 },
-
--- Bring one dilithium crystal to Bob for his 614
 {
 	mission_name = _"Ticket's price - crystal",
-        completion_code = [=[ 
+	mission_description = "Bring one dilithium crystal to Bob for his 614",
+	completion_code = [=[
 		change_obstacle_state("ToTheWorld2PortalGate", "opened")
 		add_xp(1000)
 	]=],
 },
-
--- Bring 999 Cerebrum some add-ons for his needs
 {
 	mission_name = _"Bot in need",
-	completion_ccode = [=[
+	mission_description = "Bring 999 Cerebrum some add-ons for his needs",
+	completion_code = [=[
 		add_xp(500)
 	]=],
 },
