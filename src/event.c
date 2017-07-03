3f1d1c444e620f1a2456cb4eb3ec2149a807a0e5
fluzz <fluzz@gmx.com>
2017-07-03 14:42:35 +0200

    Event validation (benchmark): use dynarray_member() instead of a direct access to the array elements.

--- src/event.c
+++ src/event.c
@@ -704,15 +704,10 @@ int validate_events()
 		run_lua(LUA_DIALOG, "function exit_game(a)\nend\n");
 
 		// Loop on all events
-		int i;
-
-		// <fluzz> already done in prepare_start_of_new_game()...
-		GetEventTriggers("events.dat");
-		struct event_trigger *arr = event_triggers.arr;
-
-		for (i = 0; i < event_triggers.size; i++) {
-			printf("Testing event \"%s\" from  \"%s\"...\n", arr[i].name, act->name);
-			int rtn = run_lua(LUA_DIALOG, arr[i].lua_code);
+		for (int i = 0; i < event_triggers.size; i++) {
+			struct event_trigger *evt = (struct event_trigger *)dynarray_member(&event_triggers, i, sizeof(struct event_trigger));
+			printf("Testing event \"%s\" from  \"%s\"...\n", evt->name, act->name);
+			int rtn = run_lua(LUA_DIALOG, evt->lua_code);
 			if (rtn)
 				error_caught = TRUE;
 			if (term_has_color_cap)
